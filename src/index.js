0ddd150065ec1cfb0c18539b7ed48f88b1529cb6
Alexey Ardov <a.ardov@semrush.com>
2019-01-04 18:11:03 +0300

    basic styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import 'normalize.css'
-// import './index.css'
+import './index.css'
+
 import App from './App'
 import Store from './Store/'
 import * as serviceWorker from './serviceWorker'
