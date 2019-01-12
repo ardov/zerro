d7498567a97264dabf2f6d6acd2af70c764be835
Alexey Ardov <a.ardov@semrush.com>
2019-01-12 22:44:14 +0300

    fix saving to local storage

--- src/Store/reducers/serverData.js
+++ src/Store/reducers/serverData.js
@@ -24,20 +24,23 @@ export default function serverData(state = defaultState, action = {}) {
     case types.MERGE_SERVER_DATA:
       const newState = parseData(state, payload)
       LocalStorage.set('data', {
-        lastSync: state.lastSync,
-        instrument: state.instrument,
-        country: state.country,
-        company: state.company,
-        user: state.user,
-        account: state.account,
-        tag: state.tag,
-        // budget: state.budget,
-        merchant: state.merchant,
-        reminder: state.reminder,
-        reminderMarker: state.reminderMarker,
-        transaction: state.transaction
+        lastSync: newState.lastSync,
+        instrument: newState.instrument,
+        country: newState.country,
+        company: newState.company,
+        user: newState.user,
+        account: newState.account,
+        tag: newState.tag,
+        // budget: newState.budget,
+        merchant: newState.merchant,
+        reminder: newState.reminder,
+        reminderMarker: newState.reminderMarker,
+        transaction: newState.transaction
       })
       return newState
+
+    case types.SET_LOCAL_STATE:
+      return payload
 
     default:
       return state
