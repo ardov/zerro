815fffc578eaecf7153f257cacb6ab97aca3406c
Alexey Ardov <ardovalexey@gmail.com>
2019-05-05 20:49:36 +0300

    add initial state

--- src/Store/fakeTransactions.js
+++ src/Store/fakeTransactions.js
@@ -5,19 +5,17 @@ export const addFakeTransaction = createAction('fakeTransactions/add')
 export const removeFakeTransaction = createAction('fakeTransactions/remove')
 
 //REDUCER
-export default createReducer(
-  {},
-  {
-    [addFakeTransaction]: (state, action) => ({
-      ...state,
-      [action.payload.id]: action.payload
-    }),
-    [removeFakeTransaction]: (state, action) => ({
-      ...state,
-      [action.payload.id]: undefined
-    })
-  }
-)
+const initialState = {}
+export default createReducer(initialState, {
+  [addFakeTransaction]: (state, action) => ({
+    ...state,
+    [action.payload.id]: action.payload
+  }),
+  [removeFakeTransaction]: (state, action) => ({
+    ...state,
+    [action.payload.id]: undefined
+  })
+})
 
 //SELECTOR
 export const getFakeTransaction = (state, id) => state.fakeTransaction[id]
