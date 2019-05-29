84bc5e2ec3fe76532369938fdfd909c8c3aec0dc
Alexey Ardov <ardovalexey@gmail.com>
2019-05-29 22:30:21 +0300

    add deselect all and better text

--- src/containers/BulkActions.js
+++ src/containers/BulkActions.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
-import { getSelectedIds } from '../store/selectedTransactions'
+import {
+  getSelectedIds,
+  uncheckAllTransactions
+} from '../store/selectedTransactions'
 import {
   setMainTagToTransactions,
   deleteTransactions
 } from '../store/data/thunks'
 import { Button } from 'antd'
 import TagSelect from './TagSelect'
+import pluralize from '../Utils/pluralize'
 
 const Num = styled.h3`
   text-align: center;
@@ -15,6 +19,10 @@ const Num = styled.h3`
 `
 
 const StyledButton = styled(Button)`
+  margin-top: 16px;
+`
+
+const StyledSelect = styled(TagSelect)`
   margin-top: 16px;
 `
 
@@ -27,17 +35,27 @@ class BulkActions extends React.Component {
   }
 
   render() {
-    if (!this.props.selectedIds.length) return null
+    const num = this.props.selectedIds.length
+
+    if (!num) return null
     return (
       <div className={this.props.className}>
-        <Num>Выбрано: {this.props.selectedIds.length}</Num>
-        <TagSelect
+        <Num>
+          {num} {pluralize(num, ['операция', 'операции', 'операций'])}
+        </Num>
+
+        <StyledButton type="link" block onClick={this.props.uncheckAll}>
+          Снять выделение
+        </StyledButton>
+
+        <StyledSelect
           single
           onChange={this.onSetTag}
           placeholder="Выставить категорию"
         />
+
         <StyledButton type="danger" block onClick={this.onDelete}>
-          Удалить выбранные({this.props.selectedIds.length})
+          Удалить выбранные ({num})
         </StyledButton>
       </div>
     )
@@ -46,7 +64,8 @@ class BulkActions extends React.Component {
 
 const mapDispatchToProps = dispatch => ({
   setTag: (ids, tagId) => dispatch(setMainTagToTransactions(ids, tagId)),
-  delete: ids => dispatch(deleteTransactions(ids))
+  delete: ids => dispatch(deleteTransactions(ids)),
+  uncheckAll: () => dispatch(uncheckAllTransactions())
 })
 
 export default connect(
