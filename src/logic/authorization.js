31c9dcbeb7a6c11bebed8f3b65a578e6d59fd168
Alexey Ardov <ardovalexey@gmail.com>
2019-05-29 21:46:06 +0300

    [fix] sync after log in

--- src/logic/authorization.js
+++ src/logic/authorization.js
@@ -2,14 +2,15 @@ import LocalStorage from '../services/localstorage'
 import Cookies from 'cookies-js'
 import ZenApi from '../services/ZenApi'
 import { setToken } from '../store/token'
-import { updateData, wipeData } from '../store/data'
+import { wipeData } from '../store/data'
+import { syncData } from '../store/data/thunks'
 
 export const logIn = () => (dispatch, getState) => {
   dispatch(logOut())
   ZenApi.getToken()
     .then(token => {
       dispatch(setToken(token))
-      dispatch(updateData())
+      dispatch(syncData())
     })
     .catch(err => console.warn('!!! Login failed', err))
 }
