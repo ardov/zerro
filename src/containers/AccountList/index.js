84645601b38bc0fe24ac0b08b88bbdc4129923c5
Alexey Ardov <ardovalexey@gmail.com>
2019-06-17 20:14:02 +0300

    show total balance

--- src/containers/AccountList/index.js
+++ src/containers/AccountList/index.js
@@ -4,13 +4,16 @@ import Account from './Account'
 import { getInBalance, getOutOfBalance } from 'store/data/selectors/accounts'
 import { getAllBudgets } from 'store/data/selectors/budgetView'
 
+const getTotalBalance = accs =>
+  accs.reduce((sum, acc) => +(sum + acc.balance).toFixed(2), 0)
+
 const AccountList = props => (
-  <div>
-    <h3>В бюджете</h3>
+  <div className={props.className}>
+    <h3>В бюджете ({getTotalBalance(props.inBalance)})</h3>
     {props.inBalance.map(acc => (
       <Account key={acc.id} {...acc} />
     ))}
-    <h3>Не в бюджете</h3>
+    <h3>Не в бюджете ({getTotalBalance(props.outOfBalance)})</h3>
     {props.outOfBalance.map(acc => (
       <Account key={acc.id} {...acc} />
     ))}
