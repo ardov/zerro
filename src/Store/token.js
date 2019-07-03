65538f899335626995104ff40b9c7d1aaf98b3e9
Alexey Ardov <ardovalexey@gmail.com>
2019-07-04 01:28:05 +0300

    add selector

--- src/Store/token.js
+++ src/Store/token.js
@@ -5,8 +5,9 @@ export const setToken = createAction('token/set')
 
 // REDUCER
 export default createReducer(null, {
-  [setToken]: (state, action) => action.payload
+  [setToken]: (state, action) => action.payload,
 })
 
 // SELECTORS
+export const getToken = state => state.token
 export const getLoginState = state => !!state.token
