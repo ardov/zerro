3343088ff8a4db28d7bb956b109c2af969fb246a
Alexey Ardov <ardovalexey@gmail.com>
2019-07-04 04:37:13 +0300

    quiet sync

--- src/Store/diff/sync.js
+++ src/Store/diff/sync.js
@@ -7,31 +7,29 @@ import { getToken } from 'store/token'
 import { removeSynced } from './actions'
 
 //All syncs with ZM goes through this thunk
-export const syncData = (changed, messages = {}) => (dispatch, getState) => {
+export const syncData = () => (dispatch, getState) => {
   const state = getState()
   const changed = getChangedArrays(state)
   const token = getToken(state)
   const serverTimestamp = state.data.serverTimestamp || 0
 
   // MESSAGES
-  const processMessage = messages.process || 'Синхронизируемся...'
-  const successMessage = messages.success || 'Готово!'
-  const failMessage = messages.fail || 'Что-то пошло не так'
+  const successMessage = 'Данные обновлены'
+  const failMessage = 'Синхронизация не удалась'
 
   const syncBegin = Date.now() / 1000
-
-  message.loading(processMessage, 0)
 
   return ZenApi.getData(token, { serverTimestamp, changed }).then(
     json => {
       dispatch(updateData(json))
       dispatch(removeSynced(syncBegin))
       LocalStorage.set('data', getState().data)
-      message.destroy()
-      message.success(successMessage)
+      if (Object.keys(json).length > 1) {
+        message.success(successMessage)
+      }
+      console.log('✅ Данные обновлены', new Date())
     },
     err => {
-      message.destroy()
       message.error(failMessage)
       console.warn('Syncing failed', err)
     }
