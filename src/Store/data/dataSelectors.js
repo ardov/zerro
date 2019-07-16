edc5201d5436ec084386bef7daddd3cad0766e39
Alexey Ardov <ardovalexey@gmail.com>
2019-07-17 01:36:14 +0300

    dataToSave selector

--- src/Store/data/dataSelectors.js
+++ src/Store/data/dataSelectors.js
@@ -1,7 +1,36 @@
 import { getBudgetsToSync } from 'store/data/budgets'
 import { getTransactionsToSync } from 'store/data/transactions'
 
+import { getBudgetsToSave } from 'store/data/budgets/'
+import { getTransactionsToSave } from 'store/data/transactions'
+import { getInstrumentsToSave } from 'store/data/instruments'
+import { getMerchantsToSave } from 'store/data/merchants'
+import { getUsersToSave } from 'store/data/users'
+import { getServerTimestampToSave } from 'store/data/serverTimestamp'
+import { getAccountsToSave } from 'store/data/accounts'
+import { getTagsToSave } from 'store/data/tags'
+import { getCountriesToSave } from 'store/data/_countries'
+import { getCompaniesToSave } from 'store/data/_companies'
+import { getRemindersToSave } from 'store/data/_reminders'
+import { getReminderMarkersToSave } from 'store/data/_reminderMarkers'
+
 // SELECTOR
+
+export const getDataToSave = state => ({
+  serverTimestamp: getServerTimestampToSave(state),
+  instrument: getInstrumentsToSave(state),
+  user: getUsersToSave(state),
+  merchant: getMerchantsToSave(state),
+  country: getCountriesToSave(state),
+  company: getCompaniesToSave(state),
+  reminder: getRemindersToSave(state),
+  reminderMarker: getReminderMarkersToSave(state),
+  account: getAccountsToSave(state),
+  tag: getTagsToSave(state),
+  budget: getBudgetsToSave(state),
+  transaction: getTransactionsToSave(state),
+})
+
 export const getChangedArrays = state => ({
   transaction: getTransactionsToSync(state),
   budget: getBudgetsToSync(state),
