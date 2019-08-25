5a6fae0c1d419b7438a0ca64bc6830b822a876a4
Alexey Ardov <ardovalexey@gmail.com>
2019-08-25 23:37:12 +0300

    no more rerenders with regular sync

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
-
 import { IntlProvider, addLocaleData } from 'react-intl'
 import ru from 'react-intl/locale-data/ru'
-
 import Transactions from 'scenes/Transactions'
 import Tags from 'scenes/Tags'
 import Auth from 'scenes/Auth'
 import Budgets from 'scenes/Budgets'
 import { getLoginState } from 'store/token'
-import { syncData } from 'logic/sync'
-import { getLastSyncTime } from 'store/data/serverTimestamp'
-import { getLastChangeTime } from 'store/data/dataSelectors'
-import { getPendingState } from 'store/isPending'
-import { loadLocalData } from 'logic/localData'
+import RegularSyncHandler from 'components/RegularSyncHandler'
 
 addLocaleData(ru)
 
-const SYNC_DELAY = 10 * 60 * 1000 // 10min
-const CHECK_DELAY = 20 * 1000 // 20sec
-let timer = null
-
-class App extends Component {
-  componentDidMount = () => {
-    this.props.loadLocalData().then(this.checkSync)
-    window.addEventListener('beforeunload', this.beforeUnload)
-  }
-
-  componentWillUnmount = () => {
-    window.removeEventListener('beforeunload', this.beforeUnload)
-    window.clearTimeout(timer)
-  }
-
-  beforeUnload = e => {
-    if (this.props.lastChange) {
-      window.alert('UNSAVED CHANGES')
-      ;(e || window.event).returnValue = null
-      return null
-    }
-  }
-
-  checkSync = () => {
-    const checkSync = this.checkSync
-    const { lastSync, sync, isLoggedIn, lastChange, isPending } = this.props
-    // Regular sync conditions
-    const needRegularSync = Date.now() - lastSync > SYNC_DELAY
-    const hasUnsavedChanges = !!lastChange
-    const itsTimeToSyncChanges = Date.now() - lastChange > CHECK_DELAY
-
-    if (
-      isLoggedIn &&
-      !isPending &&
-      (needRegularSync || (hasUnsavedChanges && itsTimeToSyncChanges))
-    ) {
-      console.log(`${needRegularSync ? 'regular' : ''}`)
-      sync()
-    }
-
-    timer = setTimeout(checkSync, CHECK_DELAY)
-  }
-
-  render() {
-    const { isLoggedIn } = this.props
-
-    return (
+const App = ({ isLoggedIn }) => (
+  <React.Fragment>
+    <RegularSyncHandler>
       <IntlProvider locale="ru">
         <BrowserRouter>
           <Switch>
@@ -90,23 +40,11 @@ class App extends Component {
           </Switch>
         </BrowserRouter>
       </IntlProvider>
-    )
-  }
-}
-
-const mapStateToProps = state => ({
-  isLoggedIn: getLoginState(state),
-  lastSync: getLastSyncTime(state),
-  lastChange: getLastChangeTime(state),
-  isPending: getPendingState(state),
-})
-
-const mapDispatchToProps = dispatch => ({
-  loadLocalData: () => dispatch(loadLocalData()),
-  sync: () => dispatch(syncData()),
-})
+    </RegularSyncHandler>
+  </React.Fragment>
+)
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  state => ({ isLoggedIn: getLoginState(state) }),
+  null
 )(App)
---
+++ src/components/RegularSyncHandler.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { getLoginState } from 'store/token'
+import { syncData } from 'logic/sync'
+import { getLastSyncTime } from 'store/data/serverTimestamp'
+import { getLastChangeTime } from 'store/data/dataSelectors'
+import { getPendingState } from 'store/isPending'
+import { loadLocalData } from 'logic/localData'
+
+const SYNC_DELAY = 10 * 60 * 1000 // 10min
+const CHECK_DELAY = 20 * 1000 // 20sec
+let timer = null
+
+class RegularSyncHandler extends React.Component {
+  componentDidMount = () => {
+    console.log('regular sync started')
+    this.props.loadLocalData().then(this.checkSync)
+    window.addEventListener('beforeunload', this.beforeUnload)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('beforeunload', this.beforeUnload)
+    window.clearTimeout(timer)
+  }
+
+  beforeUnload = e => {
+    if (this.props.lastChange) {
+      window.alert('UNSAVED CHANGES')
+      ;(e || window.event).returnValue = null
+      return null
+    }
+  }
+
+  checkSync = () => {
+    const checkSync = this.checkSync
+    const { lastSync, sync, isLoggedIn, lastChange, isPending } = this.props
+    // Regular sync conditions
+    const needRegularSync = Date.now() - lastSync > SYNC_DELAY
+    const hasUnsavedChanges = !!lastChange
+    const itsTimeToSyncChanges = Date.now() - lastChange > CHECK_DELAY
+    if (
+      isLoggedIn &&
+      !isPending &&
+      (needRegularSync || (hasUnsavedChanges && itsTimeToSyncChanges))
+    ) {
+      console.log(`${needRegularSync ? 'regular' : ''}`)
+      sync()
+    }
+    timer = setTimeout(checkSync, CHECK_DELAY)
+  }
+
+  render() {
+    console.log('RENDER HOC')
+
+    return this.props.children
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoggedIn: getLoginState(state),
+  lastSync: getLastSyncTime(state),
+  lastChange: getLastChangeTime(state),
+  isPending: getPendingState(state),
+})
+
+const mapDispatchToProps = dispatch => ({
+  sync: () => dispatch(syncData()),
+  loadLocalData: () => dispatch(loadLocalData()),
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(RegularSyncHandler)
