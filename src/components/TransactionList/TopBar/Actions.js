37e87aa57d6adb2e47ee306560ffd21494a516a9
Alexey Ardov <ardovalexey@gmail.com>
2019-09-16 23:19:12 +0300

    more actions

--- src/components/TransactionList/TopBar/Actions.js
+++ src/components/TransactionList/TopBar/Actions.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline'
+import LocalOfferOutlinedIcon from '@material-ui/icons/LocalOfferOutlined'
 import Tooltip from '@material-ui/core/Tooltip'
 import Chip from '@material-ui/core/Chip'
 import Box from '@material-ui/core/Box'
 import pluralize from 'helpers/pluralize'
 import Confirm from 'components/Confirm'
+import TagSelect2 from 'components/TagSelect2'
 
 export default function Actions({ selectedIds, setTags, uncheckAll, ...rest }) {
   return (
     <Box {...rest}>
       <Chip
-        label={`${selectedIds.length} ${pluralize(selectedIds.length, [
+        label={`${pluralize(selectedIds.length, [
+          'Выбрана',
+          'Выбрано',
+          'Выбрано',
+        ])} ${selectedIds.length} ${pluralize(selectedIds.length, [
           'операция',
           'операции',
           'операций',
@@ -33,6 +39,16 @@ export default function Actions({ selectedIds, setTags, uncheckAll, ...rest }) {
           <IconButton children={<DeleteOutlineIcon />} />
         </Tooltip>
       </Confirm>
+
+      <Box ml={1} clone>
+        <TagSelect2
+          trigger={
+            <Tooltip title="Выставить категорию">
+              <IconButton children={<LocalOfferOutlinedIcon />} />
+            </Tooltip>
+          }
+        />
+      </Box>
     </Box>
   )
 }
