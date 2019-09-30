2d0b8fd2ca8d7dcf25d33bcdead495ac31662c5c
Alexey Ardov <ardovalexey@gmail.com>
2019-09-30 21:33:57 +0300

    add tag filtering

--- src/scenes/Budgets/containers/TagTable2/index.js
+++ src/scenes/Budgets/containers/TagTable2/index.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Paper } from '@material-ui/core'
 import { setOutcomeBudget } from '../../thunks'
 import { getAmountsByTag } from '../../selectors/getAmountsByTag'
 import { getUserCurrencyCode } from 'store/data/instruments'
 import Row from './Row'
 
 function TagTable({ tags, currency, date, updateBudget, ...rest }) {
+  const filtered = tags
+    .filter(tag => tag.showOutcome || tag.totalOutcome || tag.totalAvailable)
+    .sort((a, b) => a.name.localeCompare(b.name))
+
   return (
-    <div>
-      {tags.map(tag => (
+    <Paper>
+      {filtered.map(tag => (
         <Row key={tag.id} {...tag} setBudget={updateBudget} date={date}></Row>
       ))}
-    </div>
+    </Paper>
   )
 }
 
