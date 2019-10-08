d1efffd5840347a254cb56ee27dab91beeaa21b5
Alexey Ardov <ardovalexey@gmail.com>
2019-10-09 01:02:10 +0300

    mui styles

--- src/components/TransactionList/TopBar/index.js
+++ src/components/TransactionList/TopBar/index.js
@@ -1,30 +1,29 @@
 import React from 'react'
-import styled from 'styled-components'
 import Filter from './Filter'
-
-const SearchContainer = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  z-index: 3;
-  /* width: 100%; */
-  padding: 24px 16px 0;
-  background-color: #fff;
-  box-shadow: 0 16px 16px #fff;
-`
-const FilterWrapper = styled.div`
-  position: relative;
-  max-width: 560px;
-  margin: 0 auto;
-`
+import { Box } from '@material-ui/core'
 
 export default function TopBar({ style }) {
   return (
-    <SearchContainer style={style}>
-      <FilterWrapper>
-        <Filter />
-      </FilterWrapper>
-    </SearchContainer>
+    <Wrapper style={style}>
+      <Filter />
+    </Wrapper>
   )
 }
+
+const Wrapper = ({ children, ...rest }) => (
+  <Box
+    position="absolute"
+    top={0}
+    right={0}
+    left={0}
+    zIndex={3}
+    pt={3}
+    px={2}
+    bgcolor="background.default"
+    {...rest}
+  >
+    <Box position="relative" maxWidth={560} my={0} mx="auto">
+      {children}
+    </Box>
+  </Box>
+)
