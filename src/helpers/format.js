7b7c3d1b2a80f848f53cf6054daab112959ac6f6
Alexey Ardov <ardovalexey@gmail.com>
2019-11-02 02:41:23 +0300

    getCurrencySymbol

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -6,3 +6,12 @@ export function formatMoney(amount, shortCode, decimals = 2) {
     maximumFractionDigits: decimals,
   }).format(amount)
 }
+
+export function getCurrencySymbol(currency) {
+  return Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency,
+  })
+    .format(0)
+    .slice(5)
+}
