0945c16c66eb2a98ecedf2cda27dda1669da299c
Alexey Ardov <ardovalexey@gmail.com>
2019-11-02 02:40:56 +0300

    Map component

--- src/stories/DrawerComponents.stories.js
+++ src/stories/DrawerComponents.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import Reciept from 'components/DetailsDrawer/Reciept'
 import decorator from 'helpers/storybookDecorator'
+import Map from 'components/DetailsDrawer/Map'
 
 const value =
   't=20190320T2303&s=5803.00&fn=9251440300007971&i=141637&fp=4087570038&n=1'
@@ -11,6 +12,9 @@ export const actions = {
   onClose: action('onClose'),
 }
 
-storiesOf('Reciept', module)
+storiesOf('Drawer components', module)
   .addDecorator(decorator({ width: 360 - 32 }))
-  .add('default', () => <Reciept value={value} {...actions} />)
+  .add('Reciept', () => <Reciept value={value} {...actions} />)
+  .add('Map', () => (
+    <Map longitude={30.321} latitude={60.0762313} {...actions} />
+  ))
