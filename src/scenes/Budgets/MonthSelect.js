d06e7bc0d910494eaeb1fe64f72de83a9e0b3c1a
Alexey Ardov <ardovalexey@gmail.com>
2019-11-07 01:51:37 +0300

    fix month format

--- src/scenes/Budgets/MonthSelect.js
+++ src/scenes/Budgets/MonthSelect.js
@@ -6,7 +6,7 @@ import { format } from 'date-fns'
 import ru from 'date-fns/locale/ru'
 
 const getMonthName = date =>
-  format(date, 'MMMM yyyy', { locale: ru }).toUpperCase()
+  format(date, 'LLLL yyyy', { locale: ru }).toUpperCase()
 
 export default function MonthSelect({
   months,
