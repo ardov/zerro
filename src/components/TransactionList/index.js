fc3242f523e1275f577d6ffff2d9503155465245
Alexey Ardov <ardovalexey@gmail.com>
2019-11-10 00:07:02 +0300

    replace with functional component

--- src/components/TransactionList/index.js
+++ src/components/TransactionList/index.js
@@ -1,7 +1,7 @@
-import React, { memo, forwardRef } from 'react'
+import React, { forwardRef, useEffect, useRef } from 'react'
 import { Box } from '@material-ui/core'
 import { VariableSizeList as List } from 'react-window'
-import { areEqual } from 'react-window'
+// import { areEqual } from 'react-window'
 import AutoSizer from 'react-virtualized-auto-sizer'
 
 import { connect } from 'react-redux'
@@ -14,78 +14,70 @@ const GROUP_HEADER_HEIGHT = 48
 const TRANSACTION_HEIGHT = 72
 const PADDINGS = 16
 
-class TransactionList extends React.Component {
-  state = { listRef: {} }
+const SEARCH_HEIGHT = 72
+const PADDING_BOTTOM = 40
 
-  componentDidUpdate = (_, prevState) => {
-    if (prevState.listRef.resetAfterIndex) prevState.listRef.resetAfterIndex(0)
-  }
+function TransactionList({ groups, className, opened, setOpened }) {
+  const listRef = useRef()
 
-  setRef = r => this.setState({ listRef: r })
+  useEffect(() => {
+    if (listRef.current) listRef.current.resetAfterIndex(0)
+  }, [listRef, groups])
 
-  getItemKey = i => +this.props.groups[i].date
+  const getItemKey = i => +groups[i].date
 
-  getItemSize = (i, d) =>
+  const getItemSize = (i, d) =>
     GROUP_HEADER_HEIGHT +
-    TRANSACTION_HEIGHT * this.props.groups[i].transactions.length +
+    TRANSACTION_HEIGHT * groups[i].transactions.length +
     PADDINGS +
     16
 
-  render() {
-    const { groups, className, opened, setOpened } = this.props
-    const SEARCH_HEIGHT = 72
-    const PADDING_BOTTOM = 40
+  const innerElementType = forwardRef(({ children, style, ...rest }, ref) => (
+    <div
+      ref={ref}
+      style={{
+        ...style,
+        height: style.height + SEARCH_HEIGHT + PADDING_BOTTOM,
+        scrollbarWidth: 0,
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  ))
 
-    const innerElementType = forwardRef(({ children, style, ...rest }, ref) => (
-      <div
-        ref={ref}
-        style={{
-          ...style,
-          height: style.height + SEARCH_HEIGHT + PADDING_BOTTOM,
-          scrollbarWidth: 0,
-        }}
-        {...rest}
-      >
-        {children}
-      </div>
-    ))
+  const Row = ({ index, style }) => (
+    <TransactionGroup
+      style={{ ...style, top: style.top + SEARCH_HEIGHT + 16 }}
+      topOffset={SEARCH_HEIGHT}
+      name={formatDate(groups[index].date)}
+      transactions={groups[index].transactions}
+      {...{ opened, setOpened }}
+    />
+  )
 
-    const Row = memo(
-      ({ index, style }) => (
-        <TransactionGroup
-          style={{ ...style, top: style.top + SEARCH_HEIGHT + 16 }}
-          topOffset={SEARCH_HEIGHT}
-          name={formatDate(groups[index].date)}
-          transactions={groups[index].transactions}
-          {...{ opened, setOpened }}
-        />
-      ),
-      areEqual
-    )
-
-    return (
-      <Box position="relative" className={className}>
-        <TopBar />
-        <AutoSizer disableWidth={true}>
-          {({ height, width }) => (
-            <List
-              className="hidden-scroll"
-              ref={this.setRef}
-              innerElementType={innerElementType}
-              height={height}
-              itemCount={groups.length}
-              itemSize={this.getItemSize}
-              width={'100%'}
-              itemKey={this.getItemKey}
-              itemData={groups}
-            >
-              {Row}
-            </List>
-          )}
-        </AutoSizer>
-      </Box>
-    )
-  }
+  return (
+    <Box position="relative" className={className}>
+      <TopBar />
+      <AutoSizer disableWidth>
+        {({ height }) => (
+          <List
+            className="hidden-scroll"
+            ref={listRef}
+            innerElementType={innerElementType}
+            height={height}
+            itemCount={groups.length}
+            itemSize={getItemSize}
+            width="100%"
+            itemKey={getItemKey}
+            itemData={groups}
+          >
+            {Row}
+          </List>
+        )}
+      </AutoSizer>
+    </Box>
+  )
 }
 
 export default connect(
