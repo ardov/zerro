ffc1d0cad8b03762f5197639f0573666eb3bd282
Alexey Ardov <ardovalexey@gmail.com>
2019-11-11 03:32:54 +0300

    add GA events

--- src/helpers/sendEvent.js
+++ src/helpers/sendEvent.js
@@ -1,8 +1,21 @@
 const YMID = 56072254
 
 export default function sendEvent(event) {
+  if (!event) return
+  console.log('send', event)
+
   if (window.ym) {
     window.ym(YMID, 'reachGoal', event)
-    console.log('send ', event)
+  }
+
+  if (window.ga) {
+    const eventArr = event.split(': ')
+    window.ga(
+      'send',
+      'event',
+      eventArr[0],
+      eventArr[1] || '-',
+      eventArr[2] || ''
+    )
   }
 }
