fd5237dfd7d6cc9fe3898fc2bb5656afad604b61
Alexey Ardov <ardovalexey@gmail.com>
2019-12-18 21:09:40 +0300

    [fix] unknown currency

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -1,10 +1,22 @@
 export function formatMoney(amount, shortCode, decimals = 2) {
-  return new Intl.NumberFormat('ru', {
-    style: shortCode ? 'currency' : 'decimal',
-    currency: shortCode || undefined,
-    minimumFractionDigits: decimals,
-    maximumFractionDigits: decimals,
-  }).format(amount)
+  try {
+    return new Intl.NumberFormat('ru', {
+      style: shortCode ? 'currency' : 'decimal',
+      currency: shortCode || undefined,
+      minimumFractionDigits: decimals,
+      maximumFractionDigits: decimals,
+    }).format(amount)
+  } catch (error) {
+    return (
+      new Intl.NumberFormat('ru', {
+        style: 'decimal',
+        minimumFractionDigits: decimals,
+        maximumFractionDigits: decimals,
+      }).format(amount) +
+      ' ' +
+      shortCode
+    )
+  }
 }
 
 export function getCurrencySymbol(currency) {
