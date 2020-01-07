6196c46a244ce0eb1e0f8145cb28a5e2049374b0
Alexey Ardov <ardovalexey@gmail.com>
2020-01-07 03:14:43 +0300

    delete unnecessary

--- src/scenes/Budgets/selectors/getTagGroups.js
+++
@@ -1,7 +0,0 @@
-const TagGroup = {
-  mainTag: {}, // main tag
-  children: [{}], // can be connected accounts or tags
-  budget: 0, // sum of main and children budgets
-  outcome: 0, // sum of main and children outcome
-  available: 0, // sum of main and children available
-}
