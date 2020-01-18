6a4ee9c9ca4286578aa737a0996ebdc9cb073c17
Alexey Ardov <ardovalexey@gmail.com>
2020-01-18 23:27:07 +0300

    add vibration for mobile

--- src/scenes/Budgets/index.js
+++ src/scenes/Budgets/index.js
@@ -35,7 +35,14 @@ const Budgets = ({ monthDates, dispatch }) => {
   }
 
   return (
-    <DragDropContext onDragEnd={moveMoney}>
+    <DragDropContext
+      onDragEnd={moveMoney}
+      onDragStart={() => {
+        if (window.navigator.vibrate) {
+          window.navigator.vibrate(100)
+        }
+      }}
+    >
       <Box p={isMobile ? 1 : 3}>
         <Grid container spacing={3}>
           <Grid item xs={12} md={3}>
