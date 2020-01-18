6c8e5b9082473d8c85e742803db9e4e691aebcd3
Alexey Ardov <ardovalexey@gmail.com>
2020-01-18 19:27:09 +0300

    add drag context

--- src/scenes/Budgets/index.js
+++ src/scenes/Budgets/index.js
@@ -9,8 +9,10 @@ import MonthSelector from './MonthSelect'
 import getMonthDates from './selectors/getMonthDates'
 import { Box, Hidden, useMediaQuery } from '@material-ui/core'
 import Grid from '@material-ui/core/Grid'
+import { DragDropContext } from 'react-beautiful-dnd'
+import { moveFunds } from './thunks'
 
-const Budgets = ({ monthDates }) => {
+const Budgets = ({ monthDates, dispatch }) => {
   const isMobile = useMediaQuery(theme => theme.breakpoints.down('xs'))
   const [month, setMonth] = React.useState(+startOfMonth(new Date()))
 
@@ -18,27 +20,43 @@ const Budgets = ({ monthDates }) => {
   const setMonthByIndex = i => setMonth(monthDates[i])
   const index = monthDates.findIndex(date => date === month)
 
+  const moveMoney = e => {
+    console.log(e)
+
+    if (
+      e.source &&
+      e.destination &&
+      e.source.droppableId !== e.destination.droppableId
+    ) {
+      dispatch(
+        moveFunds(1000, e.source.droppableId, e.destination.droppableId, month)
+      )
+    }
+  }
+
   return (
-    <Box p={isMobile ? 1 : 3}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={3}>
-          <MonthSelector
-            months={monthDates}
-            current={index}
-            onSetCurrent={setCurrentMonth}
-            onChange={setMonthByIndex}
-          />
-          <Box component={BudgetInfo} index={index} mt={3} />
-          <Hidden smDown>
-            <Box component={AccountList} mt={3} />
-          </Hidden>
+    <DragDropContext onDragEnd={moveMoney}>
+      <Box p={isMobile ? 1 : 3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={3}>
+            <MonthSelector
+              months={monthDates}
+              current={index}
+              onSetCurrent={setCurrentMonth}
+              onChange={setMonthByIndex}
+            />
+            <Box component={BudgetInfo} index={index} mt={3} />
+            <Hidden smDown>
+              <Box component={AccountList} mt={3} />
+            </Hidden>
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <TagTable index={index} date={monthDates[index]} />
+            <Box component={TransferTable} index={index} mt={3} />
+          </Grid>
         </Grid>
-        <Grid item xs={12} md={9}>
-          <TagTable index={index} date={monthDates[index]} />
-          <Box component={TransferTable} index={index} mt={3} />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </DragDropContext>
   )
 }
 
