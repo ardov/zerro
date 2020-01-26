8bdba41151a3c1573a874db9d97208b3da9b7eb8
Alexey Ardov <ardovalexey@gmail.com>
2020-01-27 02:06:25 +0300

    new menu layout

--- src/components/Navigation/NavDrawer.js
+++ src/components/Navigation/NavDrawer.js
@@ -3,75 +3,127 @@ import { withRouter } from 'react-router'
 import { Link } from 'react-router-dom'
 import RefreshButton from './containers/RefreshButton'
 import MenuButton from './containers/MenuButton'
-import { Tabs, Tab, Box, Drawer, IconButton, Tooltip } from '@material-ui/core'
-import { withStyles, useTheme } from '@material-ui/styles'
+import {
+  Box,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+} from '@material-ui/core'
+import { useTheme } from '@material-ui/styles'
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney'
 import Logo from 'components/Logo'
+import AccountList from 'components/AccountList'
+import { makeStyles } from '@material-ui/styles'
+import ViewAgendaIcon from '@material-ui/icons/ViewAgenda'
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
 
 const routes = [
-  { path: '/budget', label: 'Бюджет' },
-  { path: '/transactions', label: 'История' },
+  { path: '/budget', label: 'Бюджет', icon: <AccountBalanceIcon /> },
+  { path: '/transactions', label: 'История', icon: <ViewAgendaIcon /> },
 ]
+
+const useStyles = makeStyles(theme => ({
+  listItem: { borderRadius: theme.shape.borderRadius },
+}))
 
 const NavigationDrawer = ({
   location,
   match,
   history,
   staticContext,
+  onNavigate,
   ...rest
 }) => {
+  const c = useStyles()
   const theme = useTheme()
-  const SmallTab = withStyles({ root: { minWidth: 120 } })(Tab)
   return (
     <Drawer {...rest}>
       <Box
         display="flex"
+        position="relative"
         flexDirection="column"
         alignItems="center"
         minHeight="100vh"
       >
-        <Box py={3} px={2}>
-          <Logo width="120" fill={theme.palette.text.secondary} />
-        </Box>
-
-        <Box width="100%" py={3}>
-          <Tabs
-            value={routes.findIndex(route => route.path === location.pathname)}
-            indicatorColor="primary"
-            orientation="vertical"
-            variant="scrollable"
-          >
+        <Box width="100%" px={1} pt={2}>
+          <List>
             {routes.map(route => (
-              <SmallTab
-                component={Link}
-                label={route.label}
-                to={route.path}
+              <ListItem
+                className={c.listItem}
+                button
+                selected={route.path === location.pathname}
                 key={route.path}
-              />
+                component={Link}
+                to={route.path}
+                onClick={onNavigate}
+              >
+                <ListItemIcon>{route.icon}</ListItemIcon>
+                <ListItemText primary={route.label} />
+              </ListItem>
             ))}
-            <SmallTab
-              label="О проекте"
+            <ListItem
+              className={c.listItem}
+              button
               component="a"
               href="https://www.notion.so/ZERRO-a943f930d0a64d008712e20ecd299dbd"
               target="_blank"
               rel="noopener noreferrer"
-            />
-          </Tabs>
-        </Box>
+            >
+              <ListItemIcon>
+                <HelpOutlineIcon />
+              </ListItemIcon>
+              <ListItemText primary="Как пользоваться" />
+            </ListItem>
 
-        <Box mt="auto" pt={3} pb={5} display="flex" flexDirection="column">
-          <Tooltip title="Поддержать проект">
-            <IconButton
+            <ListItem
+              className={c.listItem}
+              button
               component="a"
               href="https://money.yandex.ru/to/4100110993756505/200"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <AttachMoneyIcon />
-            </IconButton>
-          </Tooltip>
-          <RefreshButton />
-          <MenuButton />
+              <ListItemIcon>
+                <AttachMoneyIcon />
+              </ListItemIcon>
+              <ListItemText primary="Поддержать проект" />
+            </ListItem>
+          </List>
+        </Box>
+
+        <Box width="100%" py={3}>
+          <Divider />
+        </Box>
+
+        <Box width="100%" px={1}>
+          <AccountList />
+        </Box>
+
+        <Box py={4} />
+        <Box
+          bgcolor="background.paper"
+          width="100%"
+          pt={1}
+          pb={2}
+          px={3}
+          mt="auto"
+          position="sticky"
+          bottom="0"
+          left="0"
+          right="0"
+          zIndex="5"
+          display="flex"
+          flexDirection="row"
+        >
+          <Logo fill={theme.palette.primary.main} width="100" />
+          <Box ml="auto">
+            <RefreshButton />
+            <MenuButton edge="end" />
+          </Box>
         </Box>
       </Box>
     </Drawer>
