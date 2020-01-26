7c4631d98cd7c15d91780a44442c6fee8da11e61
Alexey Ardov <ardovalexey@gmail.com>
2020-01-26 16:08:47 +0300

    theme tweaks

--- src/helpers/createTheme.js
+++ src/helpers/createTheme.js
@@ -1,8 +1,14 @@
 import { createMuiTheme } from '@material-ui/core/styles'
+import { blueGrey, blue, green, grey } from '@material-ui/core/colors'
 
-const primary = { dark: '#cfd8dc', light: '#455a64' }
-const secondary = { dark: '#ff5722', light: '#ff5722' }
-const success = { dark: '#45d980', light: '#21a355' }
+const primary = { dark: blueGrey[100], light: blueGrey[800] }
+const secondary = { dark: blue[200], light: blue[700] }
+const success = { dark: green['A400'], light: green[800] }
+const background = {
+  dark: { paper: '#303030', default: '#212121' },
+  light: { paper: '#ffffff', default: grey[50] },
+}
+
 const hoverOpacity = 0.04
 const hover = {
   dark: `rgba(255, 255, 255, ${hoverOpacity})`,
@@ -10,7 +16,7 @@ const hover = {
 }
 
 export default function createTheme(type = 'light') {
-  return createMuiTheme({
+  const theme = createMuiTheme({
     palette: {
       primary: { main: primary[type] },
       secondary: { main: secondary[type] },
@@ -20,8 +26,12 @@ export default function createTheme(type = 'light') {
         hoverOpacity,
       },
       type,
+      background: background[type],
     },
     shape: { borderRadius: 8 },
     typography: { fontFamily: "'IBM Plex Sans', sans-serif" },
   })
+  console.log(theme)
+
+  return theme
 }
