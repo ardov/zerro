380c61b46fded24336e0948f157ca86ca8b4df9c
Alexey Ardov <ardovalexey@gmail.com>
2020-02-04 02:09:33 +0300

    add basic store

--- src/helpers/storybookDecorator.js
+++ src/helpers/storybookDecorator.js
@@ -4,17 +4,21 @@ import { Box } from '@material-ui/core'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
 import ru from 'date-fns/locale/ru'
+import { Provider } from 'react-redux'
+import { store } from 'store'
 
 import createTheme from 'helpers/createTheme'
 
 const decorator = boxProps => story => (
-  <ThemeProvider theme={createTheme()}>
-    <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ru}>
-      <Box m={4} {...boxProps}>
-        {story()}
-      </Box>
-    </MuiPickersUtilsProvider>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={createTheme()}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={ru}>
+        <Box m={4} {...boxProps}>
+          {story()}
+        </Box>
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
+  </Provider>
 )
 
 export default decorator
