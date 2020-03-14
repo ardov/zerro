f6b6e910315fd563071a451156f76586b41f3700
Alexey Ardov <ardovalexey@gmail.com>
2020-03-14 19:19:33 +0300

    add month select to goals popover

--- src/scenes/Budgets/containers/TagTable/GoalPopover.js
+++ src/scenes/Budgets/containers/TagTable/GoalPopover.js
@@ -1,97 +1,123 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { Box, Popover, TextField, MenuItem } from '@material-ui/core'
+import React, { useState, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  Box,
+  Popover,
+  TextField,
+  MenuItem,
+  Button,
+  IconButton,
+  Typography,
+} from '@material-ui/core'
 import AmountInput from 'components/AmountInput'
 import { getGoals, setGoal } from 'store/localData/hiddenData'
 import { GOAL_TYPES } from 'store/localData/hiddenData/constants'
+import CloseIcon from '@material-ui/icons/Close'
+import MonthSelectPopover from 'scenes/Budgets/MonthSelectPopover'
+import { format } from 'date-fns'
+import ru from 'date-fns/locale/ru'
 
 const { MONTHLY, MONTHLY_SPEND, TARGET_BALANCE } = GOAL_TYPES
 
 const amountLabels = {
-  [MONTHLY]: 'Сумма в месяц',
-  [MONTHLY_SPEND]: 'Сумма в месяц',
-  [TARGET_BALANCE]: 'Цель',
+  [MONTHLY]: 'Откладывать каждый месяц',
+  [MONTHLY_SPEND]: 'Нужно на месяц',
+  [TARGET_BALANCE]: 'Хочу накопить',
 }
 
-export function GoalPopover({
-  currency,
-  type = MONTHLY,
-  amount = 0,
-  end = '',
-  date,
-  onChange,
-  onClose,
-  ...rest
-}) {
-  const [value, setValue] = useState(amount)
-  const [vType, setVType] = useState(type)
-  const [vDate, setVDate] = useState(end)
+export default function GoalPopover({ id, currency, onClose, ...rest }) {
+  const dispatch = useDispatch()
+  const goal = useSelector(state => getGoals(state)[id] || {})
 
-  const handleTypeChange = e => setVType(e.target.value)
-  const handleDateChange = e => setVDate(e.target.value)
+  const [amount, setAmount] = useState(goal.amount)
+  const [type, setType] = useState(goal.type || MONTHLY_SPEND)
+  const [endDate, setEndDate] = useState(goal.end)
+  const popoverRef = useRef()
+  const [monthPopoverAnchor, setMonthPopoverAnchor] = useState(null)
+
+  const handleTypeChange = e => setType(e.target.value)
+  const openMonthPopover = () => setMonthPopoverAnchor(popoverRef.current)
+  const closeMonthPopover = () => setMonthPopoverAnchor(null)
+  const handleDateChange = date => {
+    closeMonthPopover()
+    setEndDate(date)
+  }
+  const removeDate = () => handleDateChange(null)
   const save = () => {
-    if (value !== amount || vType !== type || vDate !== end) {
-      onChange({ type: vType, amount: value, date, end: vDate })
+    if (amount !== goal.amount || type !== goal.type || endDate !== goal.end) {
+      dispatch(setGoal({ type, amount, end: endDate, tag: id }))
     }
     onClose()
   }
 
-  return (
-    <Popover disableRestoreFocus onClose={save} {...rest}>
-      <Box m={2}>
-        <TextField
-          select
-          variant="outlined"
-          value={vType}
-          onChange={handleTypeChange}
-          label="Тип цели"
-          fullWidth
-        >
-          <MenuItem value={MONTHLY}>Регулярные сбережения</MenuItem>
-          <MenuItem value={MONTHLY_SPEND}>Регулярные траты</MenuItem>
-          <MenuItem value={TARGET_BALANCE}>Накопить сумму</MenuItem>
-        </TextField>
-      </Box>
+  const showDateBlock = type === TARGET_BALANCE
 
-      {vType === TARGET_BALANCE && (
-        <Box m={2}>
+  return (
+    <>
+      <Popover disableRestoreFocus onClose={onClose} {...rest}>
+        <Box
+          ref={popoverRef}
+          display="grid"
+          gridRowGap={16}
+          p={2}
+          minWidth={320}
+        >
           <TextField
             select
             variant="outlined"
-            value={vDate}
-            onChange={handleDateChange}
-            label="К дате"
+            value={type}
+            onChange={handleTypeChange}
+            label="Тип цели"
             fullWidth
           >
-            <MenuItem value="">Без даты</MenuItem>
-            <MenuItem value={+new Date(2020, 8)}>сентябрь</MenuItem>
+            <MenuItem value={MONTHLY}>Регулярные сбережения</MenuItem>
+            <MenuItem value={MONTHLY_SPEND}>Регулярные траты</MenuItem>
+            <MenuItem value={TARGET_BALANCE}>Накопить сумму</MenuItem>
           </TextField>
-        </Box>
-      )}
 
-      <Box m={2}>
-        <AmountInput
-          autoFocus
-          onFocus={e => e.target.select()}
-          value={value}
-          label={amountLabels[vType]}
-          fullWidth
-          onChange={value => setValue(+value)}
-          onEnter={value => {
-            setValue(+value)
-            save()
-          }}
-          placeholder="0"
-        />
-      </Box>
-    </Popover>
+          <AmountInput
+            autoFocus
+            onFocus={e => e.target.select()}
+            value={amount}
+            label={amountLabels[type]}
+            fullWidth
+            onChange={value => setAmount(+value)}
+            onEnter={value => {
+              setAmount(+value)
+              save()
+            }}
+            placeholder="0"
+          />
+
+          {showDateBlock && (
+            <Box>
+              <Button onClick={openMonthPopover}>
+                <Typography>
+                  {endDate
+                    ? format(endDate, 'LLLL yyyy', { locale: ru }).toUpperCase()
+                    : 'Указать дату'}
+                </Typography>
+              </Button>
+              {endDate && (
+                <IconButton onClick={removeDate} children={<CloseIcon />} />
+              )}
+            </Box>
+          )}
+
+          <Button onClick={save} variant="contained" color="primary">
+            Сохранить цель
+          </Button>
+        </Box>
+      </Popover>
+
+      <MonthSelectPopover
+        open={!!monthPopoverAnchor}
+        anchorEl={monthPopoverAnchor}
+        onClose={closeMonthPopover}
+        onChange={handleDateChange}
+        value={endDate}
+        disablePast
+      />
+    </>
   )
 }
-
-const mapStateToProps = (state, { id }) => getGoals(state)[id] || {}
-
-const mapDispatchToProps = (dispatch, { id }) => ({
-  onChange: goal => dispatch(setGoal({ ...goal, tag: id })),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(GoalPopover)
