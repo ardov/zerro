862718444a2f5f025a1af8341d54a6b3f9192649
Alexey Ardov <ardovalexey@gmail.com>
2020-03-30 00:13:18 +0300

    chart changes

--- src/scenes/Stats/index.js
+++ src/scenes/Stats/index.js
@@ -1,29 +1,14 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import TransactionList from 'components/TransactionList'
-import {
-  Box,
-  Drawer,
-  useMediaQuery,
-  Typography,
-  Paper,
-} from '@material-ui/core'
+import { Box, Typography, Paper } from '@material-ui/core'
 import sendEvent from 'helpers/sendEvent'
 import { useSelector } from 'react-redux'
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from 'recharts'
+import { AreaChart, Area, Tooltip, ResponsiveContainer, Brush } from 'recharts'
 import { getTotalsByMonth } from 'scenes/Budgets/selectors/getTotalsByMonth'
 import { getAmountsByTag } from 'scenes/Budgets/selectors/getAmountsByTag'
 import { format } from 'date-fns'
 import { getAccountsHistory } from './selectors'
 import { getAccounts, getAccountList } from 'store/localData/accounts'
-import { formatMoney } from 'helpers/format'
+import { formatMoney, formatDate } from 'helpers/format'
 import Rhythm from 'components/Rhythm'
 import TransactionsDrawer from 'components/TransactionsDrawer'
 
@@ -52,7 +37,7 @@ export default function Stats() {
   console.log(accountsHistory)
   // console.log(tagData)
   // console.log(getMonthDatesFrom())
-  const startDate = +new Date(2020, 0)
+  const startDate = +new Date(2019, 0)
   const accIds = accs.filter(acc => !acc.archive).map(acc => acc.id)
 
   const onSelect = useCallback((id, date) => {
@@ -113,35 +98,91 @@ export default function Stats() {
 const AccHist = ({ id, startDate = 0, endDate, onClick }) => {
   const history = useSelector(getAccountsHistory)[id]
   const acc = useSelector(getAccounts)[id]
-  console.log(startDate)
+  const [hoverIdx, setHoverIdx] = useState(null)
+  // const [startIndex, setStartIndex] = useState(
+  //   history.findIndex(day => day.date === startDate) || 0
+  // )
 
   const data = history.filter(({ date }) => date >= startDate)
 
+  const isHovering = !!hoverIdx || hoverIdx === 0
+  const balance = isHovering ? data[hoverIdx].balance : acc.balance
+  const hoverDate = isHovering ? data[hoverIdx].date : null
+
+  const gradientOffset = () => {
+    const dataMax = Math.max(...data.map(i => i.balance))
+    const dataMin = Math.min(...data.map(i => i.balance))
+
+    if (dataMax <= 0) return 0
+    if (dataMin >= 0) return 1
+
+    return dataMax / (dataMax - dataMin)
+  }
+
+  const off = gradientOffset()
+  const colorId = 'gradient' + acc.id
+
   return (
-    <Paper style={{ overflow: 'hidden' }}>
-      <Box pt={2} px={2}>
-        <Typography variant="h5">{acc.title}</Typography>
+    <Paper style={{ overflow: 'hidden', position: 'relative' }}>
+      <Box pt={2} px={2} minWidth={160}>
+        <Typography variant="body2" onClick={() => console.log(acc)}>
+          {acc.title} {isHovering && formatDate(hoverDate)}
+        </Typography>
+        <Typography variant="h6">{formatMoney(balance)}</Typography>
       </Box>
-      <div style={{ width: '100%', height: 80 }}>
+      <div
+        style={{
+          width: '100%',
+          position: 'absolute',
+          // height: 80,
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      >
         <ResponsiveContainer>
           <AreaChart
             data={data}
             margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
             onClick={e => {
+              if (!e) return
               const date = data[e.activeLabel].date
               onClick(id, date)
             }}
+            onMouseMove={e => e && setHoverIdx(e.activeLabel ?? null)}
+            onMouseLeave={() => setHoverIdx(null)}
           >
+            <defs>
+              <linearGradient id={colorId} x1="0" y1="0" x2="0" y2="1">
+                <stop offset={off} stopColor="#000" stopOpacity={0.1} />
+                <stop offset={off} stopColor="#f00" stopOpacity={0.1} />
+              </linearGradient>
+            </defs>
             {/* <XAxis
               dataKey="date"
               tickFormatter={date => format(date, 'dd-MM-yyyy')}
             />
             <YAxis /> */}
             <Tooltip
-              labelFormatter={idx => format(data[idx].date, 'dd-MM-yyyy')}
+              active={false}
+              wrapperStyle={{ display: 'none' }}
+              labelFormatter={idx => {
+                const date = data?.[idx]?.date || 0
+                return formatDate(date)
+              }}
               formatter={val => formatMoney(val)}
+              allowEscapeViewBox={{ x: false, y: true }}
+              cursor={false}
             />
-            <Area type="monotone" dataKey="balance" />
+
+            <Area
+              type="monotone"
+              stroke="none"
+              fill={`url(#${colorId})`}
+              fillOpacity={1}
+              dataKey="balance"
+            />
           </AreaChart>
         </ResponsiveContainer>
       </div>
