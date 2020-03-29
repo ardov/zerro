f26b40853a7f308186345172b7399221c2a249e8
Alexey Ardov <ardovalexey@gmail.com>
2020-03-29 17:04:37 +0300

    move formatDate

--- src/helpers/format.js
+++ src/helpers/format.js
@@ -1,3 +1,6 @@
+import { format, isToday, isYesterday, isThisYear } from 'date-fns'
+import ru from 'date-fns/locale/ru'
+
 export function formatMoney(amount, shortCode, decimals = 2) {
   try {
     return new Intl.NumberFormat('ru', {
@@ -26,4 +29,15 @@ export function getCurrencySymbol(currency) {
   })
     .format(0)
     .slice(5)
+}
+
+export function formatDate(date, template) {
+  const opts = { locale: ru }
+
+  if (template) return format(date, template, opts)
+
+  if (isToday(date)) return format(date, 'Сегодня, d MMMM, EEEEEE', opts)
+  if (isYesterday(date)) return format(date, 'Вчера, d MMMM, EEEEEE', opts)
+  if (isThisYear(date)) return format(date, 'd MMMM, EEEEEE', opts)
+  return format(date, 'd MMMM yyyy, EEEEEE', opts)
 }
