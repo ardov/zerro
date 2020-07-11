232c1edd64722c413764f02f3960817013d4b8b0
Alexey Ardov <ardovalexey@gmail.com>
2020-07-11 17:07:15 +0300

    ts first try)

--- src/store/localData/budgets/makeBudget.ts
+++ src/store/localData/budgets/makeBudget.ts
@@ -1,16 +1,24 @@
+interface RawBudget {
+  user: number | string
+  date: number
+  tag: string | null
+  changed?: number
+  income?: number
+  incomeLock?: boolean
+  outcome?: number
+  outcomeLock?: boolean
+}
+
 export const makeBudget = ({
-  // required fields
   user,
   date,
   tag,
-
-  // optional fields
   changed = Date.now(),
   income = 0,
   incomeLock = true,
   outcome = 0,
   outcomeLock = true,
-}) => ({
+}: RawBudget): RawBudget => ({
   user,
   date,
   tag: tag === 'null' ? null : tag,
