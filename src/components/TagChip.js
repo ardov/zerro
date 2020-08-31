b8d3ab9b94ead20b72c42b39f8a8fe1da8c7af69
Alexey Ardov <ardovalexey@gmail.com>
2020-09-01 01:35:31 +0300

    change tag chip icon

--- src/components/TagChip.js
+++ src/components/TagChip.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { getPopulatedTag } from 'store/localData/tags'
 import { Chip } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
 
 function getTagLabel(state, id) {
   const tag = getPopulatedTag(state, id)
@@ -12,5 +13,5 @@ function getTagLabel(state, id) {
 
 export default function TagChip({ id, ...rest }) {
   const label = useSelector(state => getTagLabel(state, id))
-  return <Chip label={label} {...rest} />
+  return <Chip deleteIcon={<CloseIcon />} label={label} {...rest} />
 }
