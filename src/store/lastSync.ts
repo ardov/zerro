440ae885a3d4fd3be379a9715d4d72614df5744c
Alexey Ardov <ardovalexey@gmail.com>
2020-11-07 21:01:01 +0300

    last sync js -> ts

---
+++ src/store/lastSync.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { RootState } from 'store'
+
+interface LastSync {
+  finishedAt: number
+  isSuccessful: boolean | null
+  errorMessage: string | null
+}
+
+const initialState: LastSync = {
+  finishedAt: 0,
+  isSuccessful: null,
+  errorMessage: null,
+}
+
+const { reducer, actions } = createSlice({
+  name: 'lastSync',
+  initialState,
+  reducers: {
+    setSyncData: (state, action: PayloadAction<LastSync>) => ({
+      ...state,
+      ...action.payload,
+    }),
+  },
+})
+
+// REDUCER
+export default reducer
+
+// ACTIONS
+export const { setSyncData } = actions
+
+// SELECTORS
+export const getLastSyncInfo = (state: RootState) => state.lastSync
