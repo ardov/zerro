f2e50613aacbe2deb5e184567b317e1bcaa68071
Alexey Ardov <ardovalexey@gmail.com>
2020-11-08 01:14:52 +0300

    add floating time widow for chart

--- src/scenes/Budgets/containers/TagPreview/OutcomeWidget.js
+++ src/scenes/Budgets/containers/TagPreview/OutcomeWidget.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { BarChart, Bar, XAxis, ResponsiveContainer } from 'recharts'
 import { Box, Typography, useTheme } from '@material-ui/core'
 import { Amount } from '../components'
@@ -6,6 +7,12 @@ import { formatDate } from 'helpers/format'
 import { makeStyles } from '@material-ui/styles'
 import { Tooltip } from 'components/Tooltip'
 import Rhythm from 'components/Rhythm'
+import {
+  getAmountsByTag,
+  getAmountsForTag,
+} from 'scenes/Budgets/selectors/getAmountsByTag'
+import getMonthDates from 'scenes/Budgets/selectors/getMonthDates'
+import { getPopulatedTag } from 'store/localData/tags'
 
 const useStyles = makeStyles(theme => ({
   base: {
@@ -14,105 +21,30 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-const Dot = ({ color, colorOpacity = 1 }) => (
-  <span
-    style={{
-      width: 8,
-      height: 8,
-      background: color,
-      display: 'inline-block',
-      marginRight: 8,
-      borderRadius: '50%',
-      opacity: colorOpacity,
-    }}
-  />
-)
-
-function DataLine({
-  name,
-  amount,
-  currency,
-  color,
-  colorOpacity = 1,
-  tooltip,
-  ...rest
-}) {
-  return (
-    <Box display="flex" flexDirection="row" {...rest}>
-      <Box flexGrow="1" mr={1} minWidth={0} display="flex" alignItems="center">
-        <Dot color={color} colorOpacity={colorOpacity} />
-        {tooltip ? (
-          <Tooltip title={tooltip}>
-            <Typography noWrap variant="body2">
-              {name}
-            </Typography>
-          </Tooltip>
-        ) : (
-          <Typography noWrap variant="body2">
-            {name}
-          </Typography>
-        )}
-      </Box>
-      <Typography variant="body2">
-        <Amount value={amount} currency={currency} />
-      </Typography>
-    </Box>
-  )
-}
-
-const BudgetBar = ({ fill, x, y, width, height }) => {
-  return (
-    <rect
-      rx={4}
-      ry={4}
-      x={x}
-      y={y}
-      width={width * 3}
-      height={height}
-      fill={fill}
-      fillOpacity="0.2"
-    />
-  )
-}
-
-function OutcomeBar(props) {
-  const { fill, x, y, width, height, date, current } = props
-  return (
-    <>
-      <rect
-        rx={4}
-        ry={4}
-        x={x - width}
-        y={y}
-        width={width * 3}
-        height={height}
-        fill={fill}
-      />
-      {date === current && (
-        <circle
-          cx={x - width + (width * 3) / 2}
-          cy={y + height + 5}
-          r="2"
-          fill={fill}
-        />
-      )}
-    </>
-  )
-}
-
-const BudgetLine = props => {
-  const { fill, x, y, width, totalOutcome, startingAmount } = props
-  if (startingAmount >= totalOutcome) return null
-  return (
-    <rect x={x - width * 2} y={y} width={width * 3} height={1} fill={fill} />
-  )
-}
-
-export function OutcomeWidget({ data, isParent, month, ...rest }) {
+export function OutcomeWidget({ tagId, month, ...rest }) {
   const c = useStyles()
   const [selected, setSelected] = useState(month)
-  const selectedData = data.find(node => node.date === selected)
 
+  const tag = useSelector(state => getPopulatedTag(state, tagId))
+  const amounts = useSelector(getAmountsForTag)(month, tagId)
+  const allAmounts = useSelector(getAmountsByTag)
+  const dates = useSelector(getMonthDates)
+
+  const isParent = !!amounts.children
+
+  const dateRange = getDateRange(dates, 12, month)
+
+  const data = dateRange.map(month => {
+    let dataPoint = tag.parent
+      ? allAmounts[month][tag.parent].children[tagId]
+      : allAmounts[month][tagId]
+    let startingAmount = isParent
+      ? dataPoint.totalAvailable + dataPoint.totalOutcome
+      : dataPoint.available + dataPoint.outcome
+    return { ...dataPoint, date: month, startingAmount }
+  })
+
+  const selectedData = data.find(node => node.date === selected)
   useEffect(() => {
     setSelected(month)
   }, [month])
@@ -202,4 +134,123 @@ export function OutcomeWidget({ data, isParent, month, ...rest }) {
       </Box>
     </div>
   )
+}
+
+function Dot({ color, colorOpacity = 1 }) {
+  return (
+    <span
+      style={{
+        width: 8,
+        height: 8,
+        background: color,
+        display: 'inline-block',
+        marginRight: 8,
+        borderRadius: '50%',
+        opacity: colorOpacity,
+      }}
+    />
+  )
+}
+
+function DataLine({
+  name,
+  amount,
+  currency,
+  color,
+  colorOpacity = 1,
+  tooltip,
+  ...rest
+}) {
+  return (
+    <Box display="flex" flexDirection="row" {...rest}>
+      <Box flexGrow="1" mr={1} minWidth={0} display="flex" alignItems="center">
+        <Dot color={color} colorOpacity={colorOpacity} />
+        {tooltip ? (
+          <Tooltip title={tooltip}>
+            <Typography noWrap variant="body2">
+              {name}
+            </Typography>
+          </Tooltip>
+        ) : (
+          <Typography noWrap variant="body2">
+            {name}
+          </Typography>
+        )}
+      </Box>
+      <Typography variant="body2">
+        <Amount value={amount} currency={currency} />
+      </Typography>
+    </Box>
+  )
+}
+
+const BudgetBar = ({ fill, x, y, width, height }) => {
+  return (
+    <rect
+      rx={4}
+      ry={4}
+      x={x}
+      y={y}
+      width={width * 3}
+      height={height}
+      fill={fill}
+      fillOpacity="0.2"
+    />
+  )
+}
+
+function OutcomeBar(props) {
+  const { fill, x, y, width, height, date, current } = props
+  return (
+    <>
+      <rect
+        rx={4}
+        ry={4}
+        x={x - width}
+        y={y}
+        width={width * 3}
+        height={height}
+        fill={fill}
+      />
+      {date === current && (
+        <circle
+          cx={x - width + (width * 3) / 2}
+          cy={y + height + 5}
+          r="2"
+          fill={fill}
+        />
+      )}
+    </>
+  )
+}
+
+const BudgetLine = props => {
+  const { fill, x, y, width, totalOutcome, startingAmount } = props
+  if (startingAmount >= totalOutcome) return null
+  return (
+    <rect x={x - width * 2} y={y} width={width * 3} height={1} fill={fill} />
+  )
+}
+
+function getDateRange(dates, range, targetMonth) {
+  const idx = dates.findIndex(d => d === targetMonth)
+  const arrayToTrim =
+    idx === dates.length - 1 ? dates : dates.slice(0, dates.length - 1)
+  if (idx === -1) return trimArray(arrayToTrim, range)
+  return trimArray(arrayToTrim, range, idx)
+}
+
+/** Cuts out a range with target index in center */
+function trimArray(arr = [], range = 1, targetIdx) {
+  if (arr.length <= range) return arr
+  if (targetIdx === undefined) return arr.slice(-range)
+
+  let padLeft = Math.floor((range - 1) / 2)
+  let padRight = range - 1 - padLeft
+  let rangeStart = targetIdx - padLeft
+  let rangeEnd = targetIdx + padRight
+
+  if (rangeEnd >= arr.length) return arr.slice(-range)
+  if (rangeStart <= 0) return arr.slice(0, range)
+  return arr.slice(rangeStart, rangeEnd + 1)
 }
--- src/scenes/Budgets/containers/TagPreview/index.js
+++ src/scenes/Budgets/containers/TagPreview/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import getMonthDates from 'scenes/Budgets/selectors/getMonthDates'
 import EmojiIcon from 'components/EmojiIcon'
 import { Box, Typography, IconButton } from '@material-ui/core'
 import { Tooltip } from 'components/Tooltip'
 import CloseIcon from '@material-ui/icons/Close'
 import { getPopulatedTag } from 'store/localData/tags'
 import { Total, Line as TextLine } from '../components'
-import {
-  getAmountsForTag,
-  getAmountsByTag,
-} from 'scenes/Budgets/selectors/getAmountsByTag'
+import { getAmountsForTag } from 'scenes/Budgets/selectors/getAmountsByTag'
 import Rhythm from 'components/Rhythm'
 import { useMonth } from 'scenes/Budgets/useMonth'
 import { LinkedAccs } from './LinkedAccs'
@@ -36,22 +32,8 @@ export function TagPreview({ onClose, id }) {
   const [month] = useMonth()
   const tag = useSelector(state => getPopulatedTag(state, id))
   const amounts = useSelector(getAmountsForTag)(month, id)
-  const allAmounts = useSelector(getAmountsByTag)
-  const dates = useSelector(getMonthDates)
   if (!amounts) return null
   const isParent = !!amounts.children
-
-  const tagHistory = dates
-    .map(month => {
-      let dataPoint = tag.parent
-        ? allAmounts[month][tag.parent].children[id]
-        : allAmounts[month][id]
-      let startingAmount = isParent
-        ? dataPoint.totalAvailable + dataPoint.totalOutcome
-        : dataPoint.available + dataPoint.outcome
-      return { ...dataPoint, date: month, startingAmount }
-    })
-    .splice(-13, 12) // year except last empty month
 
   const {
     // available,
@@ -88,7 +70,7 @@ export function TagPreview({ onClose, id }) {
       </Box>
 
       <Rhythm gap={1.5} px={3} mt={3}>
-        <OutcomeWidget data={tagHistory} isParent={isParent} month={month} />
+        <OutcomeWidget tagId={id} month={month} />
         <TextLine
           name="Остаток с прошлого месяца"
           amount={isParent ? totalLeftover : leftover}
