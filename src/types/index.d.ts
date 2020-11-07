e5e29baec8823a9ecc0593f060287bf096470a13
Alexey Ardov <ardovalexey@gmail.com>
2020-11-07 18:09:47 +0300

    add country typings

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,6 +3,7 @@ export type Modify<T, R> = Omit<T, keyof R> & R
 export type UserId = number
 export type InstrumentId = number
 export type CompanyId = number
+export type CountryId = number
 export type TagId = string | null
 export type MerchantId = string
 export type ReminderId = string
@@ -39,7 +40,14 @@ export interface Company {
   title: string
   fullTitle: string
   www: string
-  country: string
+  country: CountryId
+}
+
+export interface Country {
+  id: CountryId
+  title: string
+  currency: InstrumentId
+  domain: string
 }
 
 export interface User {
@@ -210,8 +218,8 @@ export interface Goal {
 }
 export interface ZmGoal extends Modify<Goal, { end?: string }> {}
 
-interface ZmDeletionObject {
-  id: string
+export interface ZmDeletionObject {
+  id: string | number
   object: ObjectClass
   stamp: number
   user: number
