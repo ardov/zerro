330d27ad969ca715d651b899ab5b00ff99092dcd
Alexey Ardov <ardovalexey@gmail.com>
2020-11-22 16:26:37 +0300

    tag list optimisations

--- src/components/TransactionList/TopBar/BulkEditModal.js
+++ src/components/TransactionList/TopBar/BulkEditModal.js
@@ -8,12 +8,8 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import { useSelector, useDispatch } from 'react-redux'
 import { getTransactions } from 'store/localData/transactions'
 import { getType } from 'store/localData/transactions/helpers'
-import { Box, IconButton } from '@material-ui/core'
-import TagSelect2 from 'components/TagSelect2'
-import TagChip from 'components/TagChip'
-import { Tooltip } from 'components/Tooltip'
-import AddIcon from '@material-ui/icons/Add'
 import { setTagsToTransactions } from 'store/localData/transactions/thunks'
+import { TagList } from 'components/TagList'
 
 function isSameTags(list = []) {
   return list
@@ -21,17 +17,6 @@ function isSameTags(list = []) {
     .every((tags, i, arr) => tags === arr[0])
 }
 
-function isSameMain(list = []) {
-  return list.map(tr => tr.tag?.[0]).every((tags, i, arr) => tags === arr[0])
-}
-function isSameAdditional(list = []) {
-  return list
-    .map(tr => {
-      const [main, ...additional] = tr.tag
-      return JSON.stringify(additional)
-    })
-    .every((tags, i, arr) => tags === arr[0])
-}
 function equalArrays(a, b) {
   return JSON.stringify(a) === JSON.stringify(b)
 }
@@ -42,25 +27,26 @@ function getTypes(list = []) {
   return res
 }
 
-export default function BulkEditModal({ ids, onClose, open = false, ...rest }) {
+export default function BulkEditModal({
+  ids,
+  onClose,
+  open = false,
+  keepMounted = false,
+  ...rest
+}) {
   const dispatch = useDispatch()
   const allTransactions = useSelector(getTransactions)
   const transactions = ids.map(id => allTransactions[id]).filter(Boolean)
   const sameTags = isSameTags(transactions)
   const types = getTypes(transactions)
+  const tagType = types.income ? (types.outcome ? false : 'income') : 'outcome'
   const commonTags = sameTags ? transactions[0]?.tag || [] : ['mixed']
 
   const [tags, setTags] = useState(commonTags)
 
   useEffect(() => {
-    setTags(commonTags)
-  }, [setTags, ids])
-
-  const removeTag = removeId =>
-    tags && setTags(tags.filter(id => id !== removeId))
-  const replaceTag = (oldId, newId) =>
-    tags && setTags(tags.map(id => (id === oldId ? newId : id)))
-  const addTag = id => setTags(tags ? [...tags, id] : [id])
+    if (open) setTags(commonTags)
+  }, [setTags, ids, open])
 
   const onSave = () => {
     if (!equalArrays(commonTags, tags))
@@ -78,38 +64,16 @@ export default function BulkEditModal({ ids, onClose, open = false, ...rest }) {
             ? 'Категории'
             : 'Пока здесь переводы не поддерживаются'}
         </DialogContentText>
+
         {types.transfer === 0 && (
-          <Box p={2} bgcolor="background.default" borderRadius="borderRadius">
-            {tags.map(id => (
-              <TagSelect2
-                key={id}
-                onChange={newId => replaceTag(id, newId)}
-                exclude={tags}
-                // tagType={type}
-                trigger={
-                  <Box mr={1} my={0.5} display="inline-block">
-                    <TagChip id={id} onDelete={() => removeTag(id)} />
-                  </Box>
-                }
-              />
-            ))}
-            <TagSelect2
-              onChange={id => addTag(id)}
-              exclude={tags}
-              // tagType={type}
-              trigger={
-                <Box my={0.5} display="inline-block">
-                  <Tooltip title="Добавить категорию">
-                    <IconButton
-                      edge="end"
-                      size="small"
-                      children={<AddIcon fontSize="inherit" />}
-                    />
-                  </Tooltip>
-                </Box>
-              }
-            />
-          </Box>
+          <TagList
+            tags={tags}
+            tagType={tagType}
+            onChange={setTags}
+            p={2}
+            bgcolor="background.default"
+            borderRadius="borderRadius"
+          />
         )}
       </DialogContent>
 
