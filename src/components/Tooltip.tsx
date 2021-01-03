6dc782583545f10b321fa916c98e96826ca97017
Alexey Ardov <ardovalexey@gmail.com>
2021-01-03 07:59:02 +0300

    add more types

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { Tooltip as MaterialTooltip } from '@material-ui/core'
+import { Tooltip as MaterialTooltip, TooltipProps } from '@material-ui/core'
 
 const StyledTooltip = withStyles(theme => ({
   tooltip: { fontSize: theme.typography.fontSize },
 }))(MaterialTooltip)
 
-export const Tooltip = props => <StyledTooltip enterDelay={300} {...props} />
+export const Tooltip = (props:TooltipProps) => <StyledTooltip enterDelay={300} {...props} />
