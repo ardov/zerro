fae627ec86ec948f71c917c72da3bb46004d35fc
Alexey Ardov <ardovalexey@gmail.com>
2021-01-10 22:09:54 +0300

    refactor account list

--- src/components/AccountList/index.tsx
+++ src/components/AccountList/index.tsx
@@ -1,59 +1,41 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {
-  getInBudgetAccounts,
-  getSavingAccounts,
-} from 'store/localData/accounts'
-import {
-  getUserInstrument,
-  getInstruments,
-  convertCurrency,
-  getUserInstrumentId,
-} from 'store/serverData'
+import { getAccountList } from 'store/localData/accounts'
+import { getUserInstrument } from 'store/serverData'
 import pluralize from 'helpers/pluralize'
-import { List } from '@material-ui/core'
+import { List, ListItem } from '@material-ui/core'
 import { Account, Subheader } from './components'
-import { round } from 'helpers/currencyHelpers'
-import { Account as AccType } from 'types'
+import { PopulatedAccount } from 'types'
 import { setInBudget } from 'store/localData/accounts/thunks'
+import { Amount } from 'components/Amount'
 
 export default function AccountList({ className = '' }) {
   const dispatch = useDispatch()
-  const convert = useSelector(convertCurrency)
-  const instruments = useSelector(getInstruments)
+  const accounts = useSelector(getAccountList)
   const userInstrument = useSelector(getUserInstrument)
-  const userInstrumentId = useSelector(getUserInstrumentId)
 
-  const getTotalBalance = (accs: AccType[]) => {
-    let sum = 0
-    accs.forEach(
-      a => (sum += convert(a.balance, a.instrument, userInstrumentId))
-    )
-    return round(sum)
-  }
-  const compare = (a: AccType, b: AccType) => {
-    const aBalance = convert(a.balance, a.instrument, userInstrumentId)
-    const bBalance = convert(b.balance, b.instrument, userInstrumentId)
-    return bBalance - aBalance
-  }
+  const getTotalBalance = (accs: PopulatedAccount[]) =>
+    accs.reduce((sum, a) => sum + a.convertedBalance, 0)
 
-  const inBudget = useSelector(getInBudgetAccounts).sort(compare)
-  const savings = useSelector(getSavingAccounts).sort(compare)
+  const inBudget = accounts.filter(a => a.inBudget)
+  const savings = accounts.filter(a => !a.inBudget && a.type !== 'debt')
 
   const inBudgetArchived = inBudget.filter(a => a.archive)
   const inBudgetActive = inBudget.filter(a => !a.archive)
   const inBudgetArchivedSum = getTotalBalance(inBudgetArchived)
   const inBudgetActiveSum = getTotalBalance(inBudgetActive)
-
-  // const savingsArchived = savings.filter(a => a.archive)
   const savingsActive = savings.filter(a => !a.archive)
-  // const savingsArchivedSum = getTotalBalance(savingsArchived)
-  // const savingsActiveSum = getTotalBalance(savingsActive)
 
   const [showArchived, setShowArchived] = useState(!!inBudgetArchivedSum)
   const savingsSum = getTotalBalance(savings)
 
-  if (!userInstrumentId || !userInstrument) return null
+  const toggleInBalance = useCallback(
+    (id: string, inBalance: boolean) => () =>
+      dispatch(setInBudget(id, inBalance)),
+    [dispatch]
+  )
+
+  // if (!userInstrumentId || !userInstrument) return null
   return (
     <div className={className}>
       <List dense>
@@ -66,37 +48,28 @@ export default function AccountList({ className = '' }) {
         {inBudgetActive.map(acc => (
           <Account
             key={acc.id}
+            account={acc}
             button
-            title={acc.title}
-            amount={acc.balance}
-            currency={instruments[acc.instrument].shortTitle}
-            onClick={() => console.log(acc)}
-            onDoubleClick={() => dispatch(setInBudget(acc.id, false))}
+            onDoubleClick={toggleInBalance(acc.id, false)}
           />
         ))}
         {!!inBudgetArchivedSum && !showArchived && (
-          <Account
-            title={`${
-              inBudgetArchived.length
-            } ${pluralize(inBudgetArchived.length, [
+          <ListItem>
+            {`${inBudgetArchived.length} ${pluralize(inBudgetArchived.length, [
               'архивный счёт',
               'архивных счёта',
               'архивных счётов',
-            ])}`}
-            amount={inBudgetArchivedSum}
-            currency={userInstrument.shortTitle}
-          />
+            ])} на `}
+            <Amount value={inBudgetArchivedSum} instrument="user" />
+          </ListItem>
         )}
         {showArchived &&
           inBudgetArchived.map(acc => (
             <Account
               key={acc.id}
+              account={acc}
               button
-              title={acc.title}
-              amount={acc.balance}
-              currency={instruments[acc.instrument].shortTitle}
-              onClick={() => console.log(acc)}
-              onDoubleClick={() => dispatch(setInBudget(acc.id, false))}
+              onDoubleClick={toggleInBalance(acc.id, false)}
             />
           ))}
       </List>
@@ -105,16 +78,14 @@ export default function AccountList({ className = '' }) {
         <Subheader
           title="Прочее"
           amount={savingsSum}
-          currency={userInstrument.shortTitle}
+          currency={userInstrument?.shortTitle}
         />
         {savingsActive.map(acc => (
           <Account
             key={acc.id}
+            account={acc}
             button
-            title={acc.title}
-            amount={acc.balance}
-            currency={instruments[acc.instrument].shortTitle}
-            onDoubleClick={() => dispatch(setInBudget(acc.id, true))}
+            onDoubleClick={toggleInBalance(acc.id, true)}
           />
         ))}
       </List>
