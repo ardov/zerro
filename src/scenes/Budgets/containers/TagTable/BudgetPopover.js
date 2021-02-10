4d18d946f289041e41b7b64f92b6ac1c97668929
vitaliypo <vitalytests@yandex.ru>
2021-02-11 00:11:40 +0100

    Action added: average outcome for 12 months

--- src/scenes/Budgets/containers/TagTable/BudgetPopover.js
+++ src/scenes/Budgets/containers/TagTable/BudgetPopover.js
@@ -21,7 +21,7 @@ import { sendEvent } from 'helpers/tracking'
 
 export default function BudgetPopover({ id, month, onClose, ...rest }) {
   const prevMonth = getPrevMonthMs(month)
-
+  const prev12Months = getPrev12MonthsMs(month)
   const goal = useSelector(getGoals)?.[id]
   const goalProgress = useSelector(getGoalsProgress)?.[month]?.[id]
   const needForGoal = goalProgress?.target
@@ -30,7 +30,6 @@ export default function BudgetPopover({ id, month, onClose, ...rest }) {
   const amountsById = useSelector(getAmountsById)
   const amounts = amountsById?.[month]?.[id] || {}
   const prevAmounts = amountsById?.[prevMonth]?.[id] || {}
-
   const dispatch = useDispatch()
   const onChange = outcome => dispatch(setOutcomeBudget(outcome, month, id))
 
@@ -42,6 +41,14 @@ export default function BudgetPopover({ id, month, onClose, ...rest }) {
     : prevAmounts.totalBudgeted
   const prevOutcome = isChild ? prevAmounts.outcome : prevAmounts.totalOutcome
 
+  let prev12MonthsOutcomes = []
+  for (let i = 0; i < prev12Months.length; i++) {
+	  isChild 
+	  ? prev12MonthsOutcomes.push(amountsById?.[prev12Months[i]]?.[id]?.outcome || {})
+	  : prev12MonthsOutcomes.push(amountsById?.[prev12Months[i]]?.[id]?.totalOutcome || {})
+  }
+  const prev12MonthsAvgOutcome = getAvgMonthsOutcome(prev12MonthsOutcomes)
+  
   const [value, setValue] = React.useState(budgeted)
   const changeAndCLose = value => {
     onClose()
@@ -60,6 +67,12 @@ export default function BudgetPopover({ id, month, onClose, ...rest }) {
       amount: +needForGoal,
       selected: +value === +needForGoal,
       condition: !!goal && !!needForGoal,
+    },
+    {
+      text: 'Средний расход за последний год',
+      amount: +prev12MonthsAvgOutcome,
+      selected: +value === +prev12MonthsAvgOutcome,
+      condition: !!prev12MonthsAvgOutcome,
     },
     {
       text: 'Бюджет в прошлом месяце',
@@ -138,9 +151,34 @@ export default function BudgetPopover({ id, month, onClose, ...rest }) {
   )
 }
 
+function getPrev12MonthsMs(date) {
+  let prevMonths = []
+  let monthToAdd = date  // current month won't be added; only use it to get previous month
+  for (let i = 0; i < 12; i++) {
+    monthToAdd = getPrevMonthMs(monthToAdd)
+    prevMonths.push(monthToAdd)
+  }
+  return prevMonths
+}
+
 function getPrevMonthMs(date) {
   const current = new Date(date)
   const yyyy = current.getFullYear()
   const mm = current.getMonth() - 1
   return +new Date(yyyy, mm)
+}
+
+function getAvgMonthsOutcome(outcomes) {
+  let result = 0
+  for (let i = 0; i < outcomes.length; i++) {
+    if (typeof outcomes[i] === 'number') {
+      const outcome = parseFloat(outcomes[i])
+      result = result + outcome
+    }
+  }
+  if (result !== 0) {
+    result = result / 12
+    return result.toFixed(2)
+  }
+  return 0
 }
