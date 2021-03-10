18814d5001205e19b0067178374c0eab4413326d
Alexey Ardov <ardovalexey@gmail.com>
2021-03-10 20:54:38 +0300

    add types

--- src/helpers/useLocalStorage.ts
+++ src/helpers/useLocalStorage.ts
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 
-export function useLocalStorage(key: string, initialValue: any) {
-  // State to store our value
-  // Pass initial state function to useState so logic is only executed once
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (value: T) => void] {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      // Get from local storage by key
       const item = window.localStorage.getItem(key)
-      // Parse stored json or if none return initialValue
       return item ? JSON.parse(item) : initialValue
     } catch (error) {
-      // If error also return initialValue
       console.log(error)
       return initialValue
     }
@@ -22,10 +20,9 @@ export function useLocalStorage(key: string, initialValue: any) {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
         value instanceof Function ? value(storedValue) : value
-      // Save state
-      setStoredValue(valueToStore)
-      // Save to local storage
+
       window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      setStoredValue(valueToStore)
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error)
