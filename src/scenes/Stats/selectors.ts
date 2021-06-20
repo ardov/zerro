1c74b516cea793306be035e6df4e2769852e32b6
Alexey Ardov <ardovalexey@gmail.com>
2021-06-20 19:33:01 +0300

    fix errors for accouts without transactions

--- src/scenes/Stats/selectors.ts
+++ src/scenes/Stats/selectors.ts
@@ -1,72 +1,50 @@
 import { createSelector } from '@reduxjs/toolkit'
 import { round } from 'helpers/currencyHelpers'
-import { getType } from 'store/localData/transactions/helpers'
 import { getAccounts } from 'store/localData/accounts'
 import { getStartBalance } from 'store/localData/accounts/helpers'
 import { getTransactionsHistory } from 'store/localData/transactions'
 import { eachDayOfInterval, startOfDay } from 'date-fns'
+import { ById } from 'types'
+
+type Point = {
+  date: number
+  balance: number
+  transactions: number[]
+}
 
 export const getAccountsHistory = createSelector(
   [getTransactionsHistory, getAccounts],
   (transactions, accounts) => {
     if (!transactions?.length || !accounts) return {}
-    const changes = {}
     const firstDate = new Date(transactions[0].date)
     const currentDate = startOfDay(new Date())
     const dateArray = eachDayOfInterval({ start: firstDate, end: currentDate })
 
-    for (const id in accounts) {
+    const changes: ById<Point[]> = {}
+    Object.keys(accounts).forEach(id => {
       changes[id] = [
         {
-          date: firstDate,
+          date: +firstDate,
           balance: getStartBalance(accounts[id]),
           transactions: [],
         },
       ]
-    }
-
-    const addAmount = (amount, acc, date) => {
-      const accHistory = changes[acc]
-      const lastPoint = accHistory[accHistory.length - 1]
-      if (lastPoint.date === date) {
-        lastPoint.balance = round(lastPoint.balance + amount)
-        lastPoint.transactions.push(amount)
-      } else {
-        accHistory.push({
-          date,
-          balance: round(lastPoint.balance + amount),
-          transactions: [amount],
-        })
-      }
-    }
+    })
 
     transactions.forEach(tr => {
       const { incomeAccount, outcomeAccount, income, outcome, date } = tr
-      const type = getType(tr)
-
-      switch (type) {
-        case 'income':
-          addAmount(income, incomeAccount, date)
-          break
-
-        case 'outcome':
-          addAmount(-outcome, outcomeAccount, date)
-          break
-
-        case 'transfer':
-          addAmount(income, incomeAccount, date)
-          addAmount(-outcome, outcomeAccount, date)
-          break
-
-        default:
-          throw new Error('unsupported type ' + type)
-      }
+      if (incomeAccount) addAmount(income, incomeAccount, date)
+      if (outcomeAccount) addAmount(-outcome, outcomeAccount, date)
     })
 
-    let result = {}
+    let result: ById<Point[]> = {}
     for (const id in changes) {
       let lastValue = 0
-      const dateMap = {}
+      const dateMap: ById<{
+        date: number
+        balance: number
+        transactions: number[]
+      }> = {}
       changes[id].forEach(obj => {
         dateMap[obj.date] = obj
       })
@@ -86,5 +64,20 @@ export const getAccountsHistory = createSelector(
     }
 
     return result
+
+    function addAmount(amount: number, acc: string, date: number) {
+      const accChanges = changes[acc]
+      const lastPoint = accChanges[accChanges.length - 1]
+      if (lastPoint.date === date) {
+        lastPoint.balance = round(lastPoint.balance + amount)
+        lastPoint.transactions.push(amount)
+      } else {
+        accChanges.push({
+          date,
+          balance: round(lastPoint.balance + amount),
+          transactions: [amount],
+        })
+      }
+    }
   }
 )
