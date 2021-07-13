55c3e0a69fa3dd836002bfd9705feeffe704b1e1
Alexey Ardov <ardovalexey@gmail.com>
2021-07-13 15:47:47 +0300

    remove comments

--- src/scenes/Review/index.tsx
+++ src/scenes/Review/index.tsx
@@ -3,7 +3,6 @@ import { Box, Button, Typography } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 import './index.scss'
 import { getYearStats } from './selectors'
-// import { getAccounts, getAccountList } from 'store/localData/accounts'
 import Rhythm from 'components/Rhythm'
 import { OutcomeCard } from './cards/OutcomeCard'
 import { NoCategoryCard } from './cards/NoCategoryCard'
@@ -12,27 +11,10 @@ import { PayeeByOutcomeCard } from './cards/PayeeByOutcomeCard'
 import { PayeeByFrequencyCard } from './cards/PayeeByFrequencyCard'
 import { IncomeCard } from './cards/IncomeCard'
 import { Card } from './cards/Card'
-// import { getBalanceChanges, accountBalanceGetter } from './getAccountHistory'
 
 export default function Review() {
   const [year, setYear] = useState(2020)
   const yearStats = useSelector(getYearStats(year))
-  // const balanceChanges = useSelector(getBalanceChanges)
-  // const accs = useSelector(getAccountList)
-  // const getBalance = useSelector(accountBalanceGetter)
-  // console.log('yearStats', yearStats)
-  // console.log('balanceChanges', balanceChanges)
-
-  // const dat = accs.map(acc => ({
-  //   id: acc.id,
-  //   title: acc.title,
-  //   '2000-01-01': getBalance(acc.id, +new Date(2000, 0, 1)),
-  //   '2020-01-01': getBalance(acc.id, +new Date(2020, 0, 1)),
-  //   '2021-01-01': getBalance(acc.id, +new Date(2021, 0, 1)),
-  //   '2030-01-01': getBalance(acc.id, +new Date(2030, 0, 1)),
-  //   history: balanceChanges.byAccount[acc.id],
-  // }))
-  // console.table(dat)
 
   if (!yearStats) return null
   const { total, byTag, receipts, byPayee } = yearStats
