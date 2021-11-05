79c0915f961088a5f464406246b84195540a21d5
Alexey Ardov <ardovalexey@gmail.com>
2021-11-05 04:49:35 +0300

    update widget

--- src/scenes/Stats/InAndOut.tsx
+++ src/scenes/Stats/InAndOut.tsx
@@ -1,132 +1,195 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Paper, Card, Typography, Box, useTheme } from '@mui/material'
 import {
-    Paper,
-    Card,
-    Typography,
-  } from '@mui/material'
-import {
-    ResponsiveContainer,
-    AreaChart,
-    Area,
-    XAxis,
-    YAxis,
-    Tooltip,
-    CartesianGrid,
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
 } from 'recharts'
 import { DataLine } from 'components/DataLine'
-import { getTransactions } from 'store/localData/transactions'
-import { round } from 'helpers/currencyHelpers'
+import {
+  getHistoryStart,
+  getTransactionsHistory,
+} from 'store/localData/transactions'
 import { formatDate, formatMoney } from 'helpers/format'
+import { makeDateArray, monthStart } from 'helpers/dateHelpers'
+import { convertCurrency } from 'store/data/selectors'
+import { round } from 'helpers/currencyHelpers'
+import { getType } from 'store/localData/transactions/helpers'
 
-const startDate = new Date(2019, 0)
-
-const months = new Array();
-var nowDate = new Date();
-//nowDate.setMonth(nowDate.getMonth());
-var currDate = startDate;
-currDate = new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate())
-while (currDate < nowDate){
-    months.push({
-        date: new Date(currDate), 
-        dateStr: formatDate(currDate, 'LLLL yyyy'),
-        in: 0, 
-        out: 0
-    });
-    currDate.setMonth(currDate.getMonth()+1);
+type Point = {
+  date: Date
+  in: number
+  out: number
 }
-//console.log(months);
 
 export function InAndOut() {
-      const data = useSelector(getTransactions);
-        for (var row in data){
-            const date = new Date(data[row]['date']);
-            for (var d in months){
-                if(!data[row]['deleted'] &&   // remove deleted
-                   (data[row]['income'] === 0 || data[row]['outcome'] === 0) &&  // remove transfer between accounts
-                    date.getMonth() === months[d]['date'].getMonth() &&
-                    date.getFullYear() === months[d]['date'].getFullYear()) {
-                        months[d]['in'] = round(months[d]['in'] + data[row]['income']);
-                        months[d]['out'] = round(months[d]['out'] + data[row]['outcome']);
-                }
-            }
+  const theme = useTheme()
+  const transactions = useSelector(getTransactionsHistory)
+  const convert = useSelector(convertCurrency)
+  const historyStart = useSelector(getHistoryStart)
+  const [filterMode, setFilterMode] = useState<'lastYear' | 'all'>('lastYear')
+
+  let points: Point[] = makeDateArray(historyStart).map(date => ({
+    date,
+    in: 0,
+    out: 0,
+  }))
+
+  transactions.forEach(tr => {
+    const trType = getType(tr)
+    if (trType === 'transfer') return
+    const date = new Date(tr.date)
+    const monthDate = monthStart(date)
+    const monthNode = points.find(node => +node.date === +monthDate)
+    if (!monthNode) return
+    if (trType === 'income') {
+      monthNode.in = round(
+        monthNode.in + convert(tr.income, tr.incomeInstrument)
+      )
     }
+    if (trType === 'outcome') {
+      monthNode.out = round(
+        monthNode.out + convert(tr.outcome, tr.outcomeInstrument)
+      )
+    }
+  })
 
-    /*type TPayload = {
-        // chartType: undefined
-        color: string
-        dataKey: string
-        fill: string
-        // formatter: undefined
-        name: string
-        payload: array
-        // type: undefined
-        // unit: undefined
-        value: number
-      }*/
-          
-    return (
-        <Paper>
-            <ResponsiveContainer height={300}>
-            <AreaChart data = {months}>
-                <defs>
-                    <linearGradient id="areaIn" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="0%" stopColor="green" stopOpacity={0.4} />
-                        <stop offset="95%" stopColor="green" stopOpacity={0.1} />
-                    </linearGradient>
-                    <linearGradient id="areaOut" x1="0" y1="0" x2="0" y2="1">
-                        <stop offset="0%" stopColor="red" stopOpacity={0.4} />
-                        <stop offset="95%" stopColor="red" stopOpacity={0.1} />
-                    </linearGradient>
-                </defs>
-                <Area dataKey="in" stroke="green" fill="url(#areaIn)" />
-                <Area dataKey="out" stroke="red" fill="url(#areaOut)" />
-                <XAxis dataKey="dateStr"  style={{fontSize: '0.6rem'}} />
-                <YAxis dataKey="in" tickFormatter={(number) => formatMoney(number)} style={{fontSize: '0.6rem'}} />
-                <Tooltip content={<CustomTooltip />} />
-                <CartesianGrid opacity={0.5} vertical={false} />
-            </AreaChart>
-            </ResponsiveContainer>
-        </Paper>
-      );
-  };
+  points = points.map((node, i, arr) => {
+    const num = 1
+    const first = i - num >= 0 ? i - num : 0
+    const array = arr.slice(first + 1, i + 1)
+    const result = {
+      ...node,
+      in: array.reduce((sum, node) => sum + node.in, 0) / array.length,
+      out: array.reduce((sum, node) => sum + node.out, 0) / array.length,
+    }
+    return result
+  })
 
-  const CustomTooltip = (props: any) => {
-    const payload = props.payload
-    const active = props.active as boolean
-    if (!active || !payload?.length) return null
-    console.log(props);
-    return (
-        <Card elevation={10} sx={{ p: 2 }}>
-          <Typography variant="h6">
-            {payload[0].payload.dateStr}
-          </Typography>
-            <DataLine
-              color='green'
-              key={2}
-              name={'Доход'}
-              amount={payload[0].payload.in}
-              instrument="user"
-            />
-            <DataLine
-              color='red'
-              key={2}
-              name={'Расход'}
-              amount={payload[0].payload.out}
-              instrument="user"
-            />
-            <DataLine
-              color='black'
-              key={2}
-              name={'Чистый доход'}
-              amount={payload[0].payload.in - payload[0].payload.out}
-              instrument="user"
-            />
-          <Typography>
-              {'Расход '} {formatMoney(payload[0].payload.out / payload[0].payload.in * 100)}{'%'}
-          </Typography>
+  if (filterMode === 'lastYear') {
+    points = points.slice(-12)
+  }
 
-        </Card>
-    );
+  const colorIncome = theme.palette.success.main
+  const colorOutcome = theme.palette.error.main
+
+  return (
+    <Paper>
+      <Box p={2} minWidth="100%">
+        <Typography variant="h5">
+          Доходы и расходы{' '}
+          <span
+            style={{ color: theme.palette.secondary.main, cursor: 'pointer' }}
+            onClick={() => {
+              setFilterMode(mode => (mode === 'all' ? 'lastYear' : 'all'))
+            }}
+          >
+            {filterMode === 'all' ? 'за всё время' : 'за год'}
+          </span>
+        </Typography>
+      </Box>
+
+      <ResponsiveContainer height={300}>
+        <AreaChart data={points}>
+          <defs>
+            <linearGradient id="areaIn" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor={colorIncome} stopOpacity={0.4} />
+              <stop offset="95%" stopColor={colorIncome} stopOpacity={0.1} />
+            </linearGradient>
+            <linearGradient id="areaOut" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor={colorOutcome} stopOpacity={0.4} />
+              <stop offset="95%" stopColor={colorOutcome} stopOpacity={0.1} />
+            </linearGradient>
+          </defs>
+          <Area
+            dataKey="in"
+            name="Доход"
+            stroke={colorIncome}
+            fill="url(#areaIn)"
+          />
+          <Area
+            dataKey="out"
+            name="Расход"
+            stroke={colorOutcome}
+            fill="url(#areaOut)"
+          />
+          <XAxis
+            dataKey="date"
+            tickFormatter={tickFormatter}
+            style={{ fontSize: '0.7rem' }}
+          />
+          <YAxis
+            dataKey="in"
+            tickFormatter={number => formatMoney(number, undefined, 0)}
+            style={{ fontSize: '0.7rem' }}
+          />
+          <Tooltip content={<CustomTooltip />} />
+          <CartesianGrid opacity={0.5} vertical={false} />
+        </AreaChart>
+      </ResponsiveContainer>
+    </Paper>
+  )
 }
 
+type TPayload = {
+  // chartType: undefined
+  color: string
+  dataKey: string
+  fill: string
+  // formatter: undefined
+  name: string
+  payload: Point
+  // type: undefined
+  // unit: undefined
+  value: number
+}
+
+const CustomTooltip = (props: any) => {
+  const payload = props.payload as TPayload[]
+  const active = props.active as boolean
+  if (!active || !payload?.length) return null
+  const date = payload[0]?.payload?.date
+
+  return (
+    <Card elevation={10} sx={{ p: 2 }}>
+      <Typography variant="h6">
+        {capitalize(formatDate(date, 'LLLL yyyy'))}
+      </Typography>
+      {payload.map(v => (
+        <DataLine
+          color={v.color}
+          key={v.dataKey}
+          name={v.name}
+          amount={v.value}
+          instrument="user"
+        />
+      ))}
+      <DataLine
+        color="transparent"
+        name={'Чистый доход'}
+        amount={payload[0].payload.in - payload[0].payload.out}
+        instrument="user"
+      />
+      {/* <Typography>
+        {'Расход '}{' '}
+        {formatMoney((payload[0].payload.out / payload[0].payload.in) * 100)}
+        {'%'}
+      </Typography> */}
+    </Card>
+  )
+}
+
+function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function tickFormatter(date: Date) {
+  return date?.getMonth() === 0
+    ? formatDate(date, 'yyyy')
+    : formatDate(date, 'LLL').toUpperCase().replace('.', '')
+}
