d3a324ce6019809444e03399a29079d62e3fc0c9
Alexey Ardov <ardovalexey@gmail.com>
2022-06-18 18:58:18 +0200

    add helpers

--- src/helpers/currencyHelpers.ts
+++ src/helpers/currencyHelpers.ts
@@ -1,1 +1,9 @@
 export const round = (amount: number): number => Math.round(amount * 100) / 100
+
+export function add(...params: number[]): number {
+  return params.reduce((acc, cur) => round(acc + cur), 0)
+}
+
+export function sub(first: number, ...params: number[]): number {
+  return params.reduce((acc, cur) => round(acc - cur), first)
+}
