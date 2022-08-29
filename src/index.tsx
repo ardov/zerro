82fa9d31cb06e0d9c72fc7a7f60f5dcfdbd4fd87
Alexey Ardov <ardovalexey@gmail.com>
2022-08-29 19:00:27 +0200

    add pwa plugin

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns'
 import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import ruDateLocale from 'date-fns/locale/ru'
 import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { store } from '@store'
 import GlobalErrorBoundary from '@components/GlobalErrorBoundary'
 import { initSentry, sendEvent } from '@shared/helpers/tracking'
@@ -43,16 +43,16 @@ root.render(
 
 // Register service worker fot app to work offline.
 // Learn more here http://bit.ly/CRA-PWA
-serviceWorkerRegistration.register({
-  onUpdate: (registration: ServiceWorkerRegistration) => {
-    registration.unregister().then(() => {
-      window.location.reload()
-    })
-  },
-  onSuccess: (registration: ServiceWorkerRegistration) => {
-    sendEvent('Version Update: ' + appVersion)
-  },
-})
+// serviceWorkerRegistration.register({
+//   onUpdate: (registration: ServiceWorkerRegistration) => {
+//     registration.unregister().then(() => {
+//       window.location.reload()
+//     })
+//   },
+//   onSuccess: (registration: ServiceWorkerRegistration) => {
+//     sendEvent('Version Update: ' + appVersion)
+//   },
+// })
 
 /** `zerro` can be used in console to access state and modify data */
 function createZerroInstance(s: typeof store) {
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import mdx from '@mdx-js/rollup'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), mdx()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    mdx(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
   build: {
     outDir: 'build',
+    sourcemap: true,
   },
   envPrefix: 'REACT_APP_',
   define: {
