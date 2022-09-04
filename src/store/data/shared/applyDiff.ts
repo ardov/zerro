05470616a2643ff7bfb53c00d2d2679470cffc30
Alexey Ardov <ardovalexey@gmail.com>
2022-09-04 23:47:12 +0200

    add immutable methods

--- src/store/data/shared/applyDiff.ts
+++ src/store/data/shared/applyDiff.ts
@@ -35,3 +35,45 @@ export function applyDiff(diff: IDiff, store: TDataStore) {
       })
   }
 }
+
+/**
+ * Immutable method
+ * @param diff
+ * @param store
+ */
+export function immutableApplyDiff(diff: IDiff, store: TDataStore) {
+  const result: TDataStore = { ...store }
+  if (diff.serverTimestamp) result.serverTimestamp = diff.serverTimestamp
+  diff.deletion?.forEach(obj => {
+    // TODO: Need some TS magic to use correct id type here
+    // @ts-ignore
+    result[obj.object] = { ...store[obj.object] }
+    // @ts-ignore
+    delete result[obj.object][obj.id]
+  })
+  processKey('instrument')
+  processKey('country')
+  processKey('company')
+  processKey('user')
+  processKey('account')
+  processKey('merchant')
+  processKey('tag')
+  processKey('budget')
+  processKey('reminder')
+  processKey('reminderMarker')
+  processKey('transaction')
+
+  return result
+
+  function processKey(key: keyof IDiff) {
+    if (key === 'serverTimestamp' || key === 'deletion') return
+    if (diff[key])
+      // @ts-ignore
+      result[key] = { ...store[key] }
+    diff[key]?.forEach(el => {
+      // @ts-ignore
+      // TODO: Need some TS magic to use correct id type here
+      result[key][el.id] = el
+    })
+  }
+}
--- src/store/data/shared/mergeDiffs.ts
+++ src/store/data/shared/mergeDiffs.ts
@@ -44,3 +44,53 @@ export function mergeDiffs(target: IDiff, diff: IDiff) {
     }
   }
 }
+
+/**
+ * Adds changes from the second diff to the first diff
+ * @param target - target object will be mutated
+ * @param diff - object with changes
+ */
+export function immutableMergeDiffs(target: IDiff, diff: IDiff) {
+  const result: IDiff = { ...target }
+  if (diff.serverTimestamp) result.serverTimestamp = diff.serverTimestamp
+  if (diff.deletion) {
+    if (result.deletion) {
+      result.deletion = [...result.deletion, ...diff.deletion]
+    } else {
+      result.deletion = [...diff.deletion]
+    }
+  }
+  merge('instrument')
+  merge('country')
+  merge('company')
+  merge('user')
+  merge('account')
+  merge('merchant')
+  merge('tag')
+  merge('budget')
+  merge('reminder')
+  merge('reminderMarker')
+  merge('transaction')
+
+  return result
+
+  /**
+   * Merges all objects from diff key into target key
+   * @param key - all diff keys except serverTimestamp and deletion
+   */
+  function merge(key: keyof IDiff) {
+    if (key === 'serverTimestamp' || key === 'deletion') return
+    if (!diff[key]) return
+    if (result[key]) {
+      diff[key]?.forEach((el: any) => {
+        const id = el.id
+        // @ts-ignore
+        const filtered = result[key]?.filter((el: any) => id !== el.id)
+        result[key] = [...filtered, el]
+      })
+    } else {
+      // @ts-ignore
+      result[key] = [...diff[key]]
+    }
+  }
+}
