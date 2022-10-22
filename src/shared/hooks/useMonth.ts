599ddc84392ae6f74584839e944503d66ea49319
Alexey Ardov <ardovalexey@gmail.com>
2022-10-22 17:16:31 +0200

    update useMonth

--- src/shared/hooks/useMonth.ts
+++ src/shared/hooks/useMonth.ts
@@ -1,25 +1,18 @@
-import { useMemo } from 'react'
-import { useHistory, useLocation } from 'react-router'
+import { useCallback } from 'react'
 import { TDateDraft, TISOMonth } from '@shared/types'
 import { isISOMonth, toISOMonth } from '@shared/helpers/date'
+import { useSearchParam } from './useSearchParam'
 
 export function useMonth(): [TISOMonth, (date: TDateDraft) => void] {
-  const history = useHistory()
-  const location = useLocation()
-  const setMonth = useMemo(() => {
-    return (date: TDateDraft = new Date()) => {
-      history.push(getModifiedPath('month', toISOMonth(date)))
-    }
-  }, [history])
-  const urlMonth = new URLSearchParams(location.search).get('month')
-  const month = isISOMonth(urlMonth) ? urlMonth : toISOMonth(new Date())
-  return [month, setMonth]
-}
+  const [urlMonth, setValue] = useSearchParam('month')
 
-function getModifiedPath(key: string, value?: string | null) {
-  const url = new URL(window.location.href)
-  url.searchParams.delete(key)
-  if (value) url.searchParams.append(key, value)
-  const path = url.pathname + url.search
-  return path
+  const setMonth = useCallback(
+    (date: TDateDraft = new Date()) => {
+      setValue(toISOMonth(date))
+    },
+    [setValue]
+  )
+  const month = isISOMonth(urlMonth) ? urlMonth : toISOMonth(new Date())
+
+  return [month, setMonth]
 }
