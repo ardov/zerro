3b72fab04592b672e04be05324e4cde3e6c740d1
Alexey Ardov <ardovalexey@gmail.com>
2022-10-26 04:09:32 +0200

    fix filling goals

--- src/features/bulkActions/fillGoals/model/fillGoals.ts
+++ src/features/bulkActions/fillGoals/model/fillGoals.ts
@@ -1,22 +1,26 @@
 import { sendEvent } from '@shared/helpers/tracking'
 import { TISOMonth } from '@shared/types'
 import { AppThunk } from '@store'
-import { getMonthTotals } from '@entities/envelopeData'
 import {
   setEnvelopeBudgets,
   TEnvBudgetUpdate,
 } from '@features/setEnvelopeBudget'
+import { goalModel, goalType } from '@entities/goal'
 
 export const fillGoals =
   (month: TISOMonth): AppThunk<void> =>
   (dispatch, getState) => {
     sendEvent('Budgets: fill goals')
     let state = getState()
-    let envelopes = getMonthTotals(state)[month].envelopes
+    let goals = goalModel.get(state)[month]
     const updates: TEnvBudgetUpdate[] = []
-    Object.values(envelopes).forEach(envelope => {
-      if (!envelope.goalNeed || envelope.goalTarget === null) return
-      updates.push({ id: envelope.id, value: envelope.goalTarget, month })
+    Object.values(goals).forEach(goalInfo => {
+      if (
+        !goalInfo?.state.need ||
+        (goalInfo?.goal.type === goalType.TARGET_BALANCE && !goalInfo?.goal.end)
+      )
+        return
+      updates.push({ id: goalInfo.id, value: goalInfo.state.target, month })
     })
     dispatch(setEnvelopeBudgets(updates))
   }
