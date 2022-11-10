36c30157d5c6a6576d16e2c2052e5d4965346544
Alexey Ardov <ardovalexey@gmail.com>
2022-11-10 03:48:27 +0100

    fix fixing overspends

--- src/features/bulkActions/fixOverspend/model/fixOverspends.ts
+++ src/features/bulkActions/fixOverspend/model/fixOverspends.ts
@@ -1,4 +1,4 @@
-import { round } from '@shared/helpers/money'
+import { add } from '@shared/helpers/money'
 import { sendEvent } from '@shared/helpers/tracking'
 import { TISOMonth } from '@shared/types'
 import { AppThunk } from '@store'
@@ -12,19 +12,47 @@ export const fixOverspends =
   (month: TISOMonth): AppThunk<void> =>
   (dispatch, getState) => {
     sendEvent('Budgets: fix overspends')
-    const metrics = balances.envData(getState())[month]
-    let updates: TEnvBudgetUpdate[] = []
 
-    Object.values(metrics).forEach(m => {
-      if (!m.parent) return
-      const budgeted = m.selfBudgeted[m.currency] || 0
-      const available = m.selfAvailable[m.currency] || 0
-      if (available > 0) return
-      updates.push({
-        month,
-        id: m.id,
-        value: round(budgeted - available),
+    fixOverspendingChildren()
+    fixOverspendingParents()
+
+    function fixOverspendingChildren() {
+      const metrics = balances.envData(getState())[month]
+      let childrenUpdates: TEnvBudgetUpdate[] = []
+      Object.values(metrics).forEach(m => {
+        if (!m.parent) return
+        const budgeted = m.selfBudgeted[m.currency] || 0
+        const available = m.selfAvailable[m.currency] || 0
+        if (available > 0) return
+        if (!budgeted) return
+        childrenUpdates.push({
+          month,
+          id: m.id,
+          value: add(budgeted, -available),
+        })
       })
-    })
-    dispatch(setEnvelopeBudgets(updates))
+      if (childrenUpdates.length) dispatch(setEnvelopeBudgets(childrenUpdates))
+    }
+
+    function fixOverspendingParents() {
+      const metrics = balances.envData(getState())[month]
+      let parentUpdates: TEnvBudgetUpdate[] = []
+      Object.values(metrics).forEach(m => {
+        if (m.parent) return
+        const totalBudgeted = m.totalBudgeted[m.currency] || 0
+        const totalAvailable = m.totalAvailable[m.currency] || 0
+        const selfAvailable = m.selfAvailable[m.currency] || 0
+
+        const needForSelf = selfAvailable < 0 ? -selfAvailable : 0
+        const needForTotal = totalAvailable < 0 ? -totalAvailable : 0
+        const need = Math.max(needForSelf, needForTotal)
+        if (!need) return
+        parentUpdates.push({
+          month,
+          id: m.id,
+          value: add(totalBudgeted, need),
+        })
+      })
+      if (parentUpdates.length) dispatch(setEnvelopeBudgets(parentUpdates))
+    }
   }
