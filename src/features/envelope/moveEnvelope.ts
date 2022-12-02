d18dce3d4dcf5c07c39f117878b13a82b3f3f631
Alexey Ardov <ardovalexey@gmail.com>
2022-12-02 21:35:52 +0100

    improve group reordering

--- src/features/envelope/moveEnvelope.ts
+++ src/features/envelope/moveEnvelope.ts
@@ -1,6 +1,7 @@
 import { envelopeModel, TEnvelopeDraft } from '@entities/envelope'
 import { TEnvelopeId } from '@shared/types'
 import { AppThunk } from '@store/index'
+import { arrayMove } from './arrayMove'
 
 export function moveEnvelope(
   sourceIdx: number,
@@ -87,31 +88,4 @@ export function moveEnvelope(
         : null
     }
   }
-}
-
-/*
-
- 0 group
- 1 - parent
- 2 - - child 1
- 3 - - child 2
- 4 - - child 3
- 5 - parent
- 6 - - child 1
- 7 group
- 8 - parent
- 9 - - child 1
-10 - - child 2
-11 - - child 3
-12 - parent
-13 - - child 1
-14 - - child 2
-15 - - child 3
-
-*/
-
-function arrayMove<T>(arr: Array<T>, fromIndex: number, toIndex: number) {
-  var element = arr[fromIndex]
-  arr.splice(fromIndex, 1)
-  arr.splice(toIndex, 0, element)
 }
