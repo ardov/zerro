4301bc607d428b6f758d1b1fea22d45415cfe941
Alexey Ardov <ardovalexey@gmail.com>
2022-12-26 18:00:02 +0100

    fix new report logic

--- src/pages/Review/index.tsx
+++ src/pages/Review/index.tsx
@@ -9,21 +9,28 @@ import { PayeeByOutcomeCard } from './cards/PayeeByOutcomeCard'
 import { PayeeByFrequencyCard } from './cards/PayeeByFrequencyCard'
 import { IncomeCard } from './cards/IncomeCard'
 import { Card } from './shared/Card'
+import { NotFunCard } from './cards/NotFunCard/NotFunCard'
+
+// New report unblocks in december
+const currMonth = new Date().getMonth()
+const currYear = new Date().getFullYear()
+const startingYear = currMonth >= 11 ? currYear : currYear - 1
 
 export default function Review() {
-  const [year, setYear] = useState(new Date().getFullYear())
+  const [year, setYear] = useState(startingYear)
 
   return (
     <Box className="container">
       <Rhythm gap={2} axis="y" p={3} pb={10}>
         <CardTitle year={year} />
+        <NotFunCard year={year} />
         <IncomeCard year={year} />
         <PayeeByOutcomeCard year={year} />
         <PayeeByFrequencyCard year={year} />
         <OutcomeCard year={year} />
         <QRCard year={year} />
         <NoCategoryCard year={year} />
-        <Button onClick={() => setYear(year - 1)}>
+        <Button onClick={() => setYear(y => y - 1)}>
           А что было в прошлом году?
         </Button>
       </Rhythm>
