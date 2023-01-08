f6c70ab7bbe52d66f3bc8773baa2614ee5253304
Alexey Ardov <ardovalexey@gmail.com>
2023-01-08 07:05:32 +0100

    handle empty state

--- src/pages/Review/cards/NotFunCard/NotFunCard.tsx
+++ src/pages/Review/cards/NotFunCard/NotFunCard.tsx
@@ -58,6 +58,104 @@ export function NotFunCard(props: TCardProps) {
   const taxMonths = Math.round(taxesRatio * 12 * 10) / 10
   const workWeek = Math.round((1 - taxesRatio) * 5 * 10) / 10
 
+  const emptyCardContent = (
+    <Stack gap={2} alignItems="center" width="100%">
+      <Balancer>
+        <Typography variant="body1" align="center">
+          Нет доходов — нет налогов 😅
+        </Typography>
+      </Balancer>
+    </Stack>
+  )
+
+  const cardContent = (
+    <Stack gap={2} alignItems="center" width="100%">
+      <TaxesChart income={totalIncome} outcome={totalOutcome} />
+      <Balancer>
+        <Typography variant="body1" align="center">
+          ≈{Math.round(taxesRatio * 100)}% от вашего дохода получила Россия.
+        </Typography>
+      </Balancer>
+      <Balancer>
+        <Typography variant="body1" align="center">
+          <b>
+            {taxMonths} {pluralize(taxMonths, ['месяц', 'месяца', 'месяцев'])}
+          </b>{' '}
+          вы работали исключительно на государство. Если бы не налоги, вы могли
+          бы за ту же зарплату работать всего{' '}
+          <b>
+            {workWeek} {pluralize(workWeek, ['день', 'дня', 'дней'])}
+          </b>{' '}
+          в неделю.
+        </Typography>
+      </Balancer>
+      <Balancer>
+        <Typography variant="body1" align="center">
+          Если вы тратите столько времени на государство, значит абсолютно
+          нормально и правильно требовать от него выполнения обязательств.
+        </Typography>
+      </Balancer>
+
+      <Divider sx={{ width: '100%' }} />
+
+      <div>
+        <Typography variant="body1" align="center">
+          Россия получила от вас
+        </Typography>
+        <Typography variant="h4" align="center" className="red-gradient">
+          ≈<DisplayAmount value={totalTaxes} noShade decMode="ifOnly" />
+        </Typography>
+      </div>
+
+      <Box textAlign="center">
+        {taxes.map(info => (
+          <Box m={0.5} display="inline-block" key={info.name}>
+            <Tooltip title={info.comment}>
+              <Chip
+                variant={'outlined'}
+                label={
+                  <>
+                    {info.name} (
+                    <DisplayAmount
+                      value={info.value}
+                      noShade
+                      decMode="ifOnly"
+                    />
+                    )
+                  </>
+                }
+              />
+            </Tooltip>
+          </Box>
+        ))}
+      </Box>
+      <Divider sx={{ width: '100%' }} />
+      <Typography variant="body1" align="center">
+        <Balancer>
+          Это приблизительные цифры, подробнее можно посчитать в{' '}
+          <Link
+            color="secondary"
+            href="https://journal.tinkoff.ru/fns-loves-you/"
+            target="_blank"
+            onClick={() => sendEvent('Review: go_to_taxes_calculator')}
+          >
+            калькуляторе Тинькофф журнала
+          </Link>{' '}
+          или посмотрите вот это{' '}
+          <Link
+            color="secondary"
+            href="https://youtu.be/xL8Z1mbcQ78"
+            target="_blank"
+            onClick={() => sendEvent('Review: go_to_taxes_video')}
+          >
+            видео про налоги
+          </Link>{' '}
+          (3 мин).
+        </Balancer>
+      </Typography>
+    </Stack>
+  )
+
   return (
     <>
       <Card sx={{ position: 'relative' }}>
@@ -71,92 +169,7 @@ export function NotFunCard(props: TCardProps) {
           <SettingsIcon />
         </IconButton>
 
-        <Stack gap={2} alignItems="center" width="100%">
-          <TaxesChart income={totalIncome} outcome={totalOutcome} />
-          <Balancer>
-            <Typography variant="body1" align="center">
-              ≈{Math.round(taxesRatio * 100)}% от вашего дохода получила Россия.
-            </Typography>
-          </Balancer>
-          <Balancer>
-            <Typography variant="body1" align="center">
-              <b>
-                {taxMonths}{' '}
-                {pluralize(taxMonths, ['месяц', 'месяца', 'месяцев'])}
-              </b>{' '}
-              вы работали исключительно на государство. Если бы не налоги, вы
-              могли бы за ту же зарплату работать всего{' '}
-              <b>
-                {workWeek} {pluralize(workWeek, ['день', 'дня', 'дней'])}
-              </b>{' '}
-              в неделю.
-            </Typography>
-          </Balancer>
-          <Balancer>
-            <Typography variant="body1" align="center">
-              Если вы тратите столько времени на государство, значит абсолютно
-              нормально и правильно требовать от него выполнения обязательств.
-            </Typography>
-          </Balancer>
-
-          <Divider sx={{ width: '100%' }} />
-
-          <div>
-            <Typography variant="body1" align="center">
-              Россия получила от вас
-            </Typography>
-            <Typography variant="h4" align="center" className="red-gradient">
-              ≈<DisplayAmount value={totalTaxes} noShade decMode="ifOnly" />
-            </Typography>
-          </div>
-
-          <Box textAlign="center">
-            {taxes.map(info => (
-              <Box m={0.5} display="inline-block" key={info.name}>
-                <Tooltip title={info.comment}>
-                  <Chip
-                    variant={'outlined'}
-                    label={
-                      <>
-                        {info.name} (
-                        <DisplayAmount
-                          value={info.value}
-                          noShade
-                          decMode="ifOnly"
-                        />
-                        )
-                      </>
-                    }
-                  />
-                </Tooltip>
-              </Box>
-            ))}
-          </Box>
-          <Divider sx={{ width: '100%' }} />
-          <Typography variant="body1" align="center">
-            <Balancer>
-              Это приблизительные цифры, подробнее можно посчитать в{' '}
-              <Link
-                color="secondary"
-                href="https://journal.tinkoff.ru/fns-loves-you/"
-                target="_blank"
-                onClick={() => sendEvent('Review: go_to_taxes_calculator')}
-              >
-                калькуляторе Тинькофф журнала
-              </Link>{' '}
-              или посмотрите вот это{' '}
-              <Link
-                color="secondary"
-                href="https://youtu.be/xL8Z1mbcQ78"
-                target="_blank"
-                onClick={() => sendEvent('Review: go_to_taxes_video')}
-              >
-                видео про налоги
-              </Link>{' '}
-              (3 мин).
-            </Balancer>
-          </Typography>
-        </Stack>
+        {totalIncome ? cardContent : emptyCardContent}
       </Card>
 
       {/* Settings */}
