e979d9f5b55abfc663804a46a83aa95e54c1642c
Alexey Ardov <ardovalexey@gmail.com>
2023-02-15 00:26:45 +0100

    move sorted activity to envBalances

--- src/entities/envBalances/index.ts
+++ src/entities/envBalances/index.ts
@@ -3,14 +3,18 @@ import { getMonthList } from './1 - monthList'
 import { getRawActivity } from './1 - rawActivity'
 import { getActivity } from './2 - activity'
 import { getRatesByMonth } from './2 - rates'
+import { getSortedActivity } from './2 - sortedActivity'
 import { getEnvMetrics } from './3 - envMetrics'
 import { getMonthTotals } from './4 - monthTotals'
 
 export type { TRawActivityNode } from './1 - rawActivity'
 export type { TActivityNode } from './2 - activity'
+export type { TSortedActivityNode, TSortedActivity } from './2 - sortedActivity'
 export type { TEnvMetrics } from './3 - envMetrics'
 export type { TMonthTotals } from './4 - monthTotals'
+
 export { EnvActivity } from './1 - rawActivity'
+export { TrFilterMode } from './2 - sortedActivity'
 
 export const balances = {
   // Selectors
@@ -18,6 +22,7 @@ export const balances = {
   rates: getRatesByMonth,
   rawActivity: getRawActivity,
   activity: getActivity,
+  sortedActivity: getSortedActivity,
   envData: getEnvMetrics,
   totals: getMonthTotals,
 
@@ -26,6 +31,7 @@ export const balances = {
   useRates: () => useAppSelector(getRatesByMonth),
   useRawActivity: () => useAppSelector(getRawActivity),
   useActivity: () => useAppSelector(getActivity),
+  useSortedActivity: () => useAppSelector(getSortedActivity),
   useEnvData: () => useAppSelector(getEnvMetrics),
   useTotals: () => useAppSelector(getMonthTotals),
 }
