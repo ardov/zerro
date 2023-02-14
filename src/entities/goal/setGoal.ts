d1242fe4abdac3ea05dbe763e3d7c0240cca3529
Alexey Ardov <ardovalexey@gmail.com>
2023-02-14 23:14:11 +0100

    remove blocking goals on creating new goal

--- src/entities/goal/setGoal.ts
+++ src/entities/goal/setGoal.ts
@@ -1,25 +1,48 @@
-import { makeGoal } from './shared/helpers'
 import { sendEvent } from '@shared/helpers/tracking'
 import { TISOMonth } from '@shared/types'
+import { keys } from '@shared/helpers/keys'
 import { AppThunk } from '@store'
-import { goalStore } from './goalStore'
-import { TGoal } from './shared/types'
 import { TEnvelopeId } from '@entities/envelope'
+import { goalStore } from './goalStore'
+import { makeGoal } from './shared/helpers'
+import { TGoal } from './shared/types'
 
 export const setGoal =
   (month: TISOMonth, id: TEnvelopeId, goal?: TGoal | null): AppThunk =>
   (dispatch, getState) => {
     const state = getState()
-    const monthlyGoals = goalStore.getData(state)[month] || {}
+    const goals = goalStore.getData(state)
     const newGoal = makeGoal(goal)
-    const newGoals = { ...monthlyGoals, [id]: newGoal }
+
+    // Update goals in given month
+    const currentGoals = goals[month] || {}
+    const newGoals = { ...currentGoals, [id]: newGoal }
+    dispatch(goalStore.setData(newGoals, month))
+
+    // null-goals are blocking goals from continuing in future
+    // if we are settin a new goal we should delete all future blockers
+    if (newGoal) removeFutureGoalBlocks()
 
     // Track these events
-    if (newGoal?.amount) {
-      sendEvent(`Goals: set ${newGoal.type} goal`)
-    } else {
-      sendEvent(`Goals: delete goal`)
-    }
+    if (newGoal !== null) sendEvent(`Goals: set ${newGoal.type} goal`)
+    else sendEvent(`Goals: delete goal`)
 
-    dispatch(goalStore.setData(newGoals, month))
+    function removeFutureGoalBlocks() {
+      const futureMonths = keys(goals)
+        .sort((a, b) => a.localeCompare(b))
+        .filter(date => date > month)
+
+      for (const date of futureMonths) {
+        // Remove blocker-goal
+        if (goals[date][id] === null) {
+          const newGoals = { ...goals[date] }
+          delete newGoals[id]
+          dispatch(goalStore.setData(newGoals, date))
+          return
+        }
+
+        // If there is a set goal in future we should stop deleting null-goals
+        if (goals[date][id]) break
+      }
+    }
   }
