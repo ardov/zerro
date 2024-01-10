f4cfd2bd4fe663143a2abd1194b880ff677e65a7
Alexey Ardov <ardovalexey@gmail.com>
2024-01-10 03:01:25 +0100

    Update Redux Toolkit and React Redux versions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { configureStore, AnyAction } from '@reduxjs/toolkit'
+import { configureStore, UnknownAction } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { ThunkAction, ThunkDispatch } from 'redux-thunk'
 import { zenmoney } from '6-shared/api/zenmoney'
@@ -27,11 +27,11 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
-  AnyAction
+  UnknownAction
 >
 export type TSelector<T> = (state: RootState) => T
 
 // App hooks
-export type AppDispatch = ThunkDispatch<RootState, any, AnyAction> // typeof store.dispatch
+export type AppDispatch = ThunkDispatch<RootState, any, UnknownAction> // typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
