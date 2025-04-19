beec4053185a3c8074dd09235346d69ce4a1bd47
Arkady Borovsky <borovskyarkady@protonmail.com>
2025-04-20 01:00:50 +0300

    Stats/index.tsx cleanup

--- src/2-pages/Stats/index.tsx
+++ src/2-pages/Stats/index.tsx
@@ -1,7 +1,6 @@
 import type { TAccountId, TISODate } from '6-shared/types'
 
-import React, { useState, useCallback } from 'react'
-import { useTranslation } from 'react-i18next'
+import React, { useState, useCallback, memo } from 'react'
 import { Stack } from '@mui/system'
 
 import { useTransactionDrawer } from '3-widgets/global/TransactionListDrawer'
@@ -11,14 +10,13 @@ import { WidgetAccHistory } from './WidgetAccHistory'
 import { nextPeriod, Period } from './shared/period'
 
 export default function Stats() {
-  const { t } = useTranslation('accounts')
   const [period, setPeriod] = useState<Period>(Period.LastYear)
-  const togglePeriod = useCallback(() => setPeriod(nextPeriod), [])
+  const togglePeriod = useCallback(() => setPeriod(prevPeriod => nextPeriod(prevPeriod)), [])
   const trDrawer = useTransactionDrawer()
 
   const onSelect = useCallback((id: TAccountId, date: TISODate) => {
     trDrawer.open({ filterConditions: { account: id }, initialDate: date })
-  }, [])
+  }, [trDrawer])
 
   return (
     <Stack spacing={2} p={3} pb={10}>
