0e396006ccb397b9f737b97439a67d81b3cf95a8
Alexey Ardov <ardovalexey@gmail.com>
2025-10-21 03:40:50 +0200

    Update transaction start date in demo data

--- src/demoData/index.ts
+++ src/demoData/index.ts
@@ -268,7 +268,7 @@ const tagsChildren = {
 // skip for now
 
 // Transactions
-const since: TISODate = '1800-01-01'
+const since: TISODate = '2019-01-01'
 
 const transactions = [
   // Salary transactions
