ec0d3fc9e6e3e6690017349ba87a9f7610183a2e
Alexey Ardov <ardovalexey@gmail.com>
2026-04-30 17:38:05 +0200

    update applying diff mechamism

--- src/store/data/shared/applyDiff.ts
+++ src/store/data/shared/applyDiff.ts
@@ -1,3 +1,4 @@
+import { keys } from '6-shared/helpers/keys'
 import { TDiff } from '6-shared/types'
 import { TDataStore } from '6-shared/types'
 
@@ -7,31 +8,38 @@ import { TDataStore } from '6-shared/types'
  * @param store
  */
 export function applyDiffMutable(diff: TDiff, store: TDataStore) {
+  // Update server timestamp
   if (diff.serverTimestamp) store.serverTimestamp = diff.serverTimestamp
-  diff.deletion?.forEach(obj => {
-    // @ts-ignore
-    // TODO: Need some TS magic to use correct id type here
-    delete store[obj.object][obj.id]
-  })
-  processKey('instrument')
-  processKey('country')
-  processKey('company')
-  processKey('user')
-  processKey('account')
-  processKey('merchant')
-  processKey('tag')
-  processKey('budget')
-  processKey('reminder')
-  processKey('reminderMarker')
-  processKey('transaction')
 
-  function processKey(key: keyof TDiff) {
-    if (key === 'serverTimestamp' || key === 'deletion') return
-    if (diff[key])
-      diff[key]?.forEach(el => {
+  // Delete objects
+  if (diff.deletion) {
+    diff.deletion.forEach(obj => {
+      try {
+        // @ts-ignore
+        // TODO: Need some TS magic to use correct id type here
+        delete store[obj.object][obj.id]
+      } catch (error) {
+        console.error('Error deleting object', error, obj)
+      }
+    })
+  }
+
+  // Add created or updated objects
+  keys(diff).forEach(key => {
+    if (key === 'serverTimestamp' || key === 'deletion' || !diff[key]) return
+    if (!Array.isArray(diff[key])) {
+      console.error('Expected array for key', key, 'got', typeof diff[key])
+      return
+    }
+    if (!store[key]) store[key] = {} // For new objects
+    diff[key].forEach(el => {
+      try {
         // @ts-ignore
         // TODO: Need some TS magic to use correct id type here
         store[key][el.id] = el
-      })
-  }
+      } catch (error) {
+        console.error('Error adding object', error, el)
+      }
+    })
+  })
 }
